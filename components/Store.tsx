376abd6e55767f165ec27718c0edfaa10526e735
acc0x <ox8xxx@gmail.com>
2024-12-20 20:20:41 +0700

    Update Store.tsx

--- components/Store.tsx
+++ components/Store.tsx
@@ -1,38 +1,35 @@
 import { useContract, useNFTs } from "@thirdweb-dev/react";
-import { TOOLS_ADDRESS } from "../const/addresses";
 import Link from "next/link";
 import { Text, Button, Container, Flex, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import NFT from "./NFT";
 
-// 定义 NFT 数据类型（根据 thirdweb 提供的类型）
-interface NFTData {
-  metadata: {
-    id: string;
-    [key: string]: any; // 可根据实际 metadata 结构调整
-  };
-  [key: string]: any; // 如果有额外数据，也可以添加
+interface StoreProps {
+  contractAddress: string; // 合约地址
+  title: string; // 标题
+  description: string; // 描述文本
+  backLink: string; // 返回按钮的链接
 }
 
-const Store: React.FC = () => {
-  const { contract } = useContract(TOOLS_ADDRESS);
+const Store: React.FC<StoreProps> = ({ contractAddress, title, description, backLink }) => {
+  const { contract } = useContract(contractAddress);
   const { data: nfts, isLoading } = useNFTs(contract);
 
   return (
     <Container maxW={"1200px"}>
       <Flex direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
-        <Link href="/">
+        <Link href={backLink}>
           <Button>Back</Button>
         </Link>
       </Flex>
-      <Heading mt={"40px"}>Store</Heading>
-      <Text>Purchase tools with $CARROTS to increase your earnings.</Text>
+      <Heading mt={"40px"}>{title}</Heading>
+      <Text>{description}</Text>
       {isLoading ? (
         <Flex h={"50vh"} justifyContent={"center"} alignItems={"center"}>
           <Spinner />
         </Flex>
       ) : (
         <SimpleGrid columns={3} spacing={10}>
-          {nfts?.map((nftItem: NFTData) => (
+          {nfts?.map((nftItem) => (
             <NFT key={nftItem.metadata.id} nft={nftItem} />
           ))}
         </SimpleGrid>
