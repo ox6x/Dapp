8a177861bb12dfbf214a17b626ddcbd9125f43d9
acc0x <ox8xxx@gmail.com>
2024-12-20 21:24:11 +0700

    Update Store.tsx

--- components/Store.tsx
+++ components/Store.tsx
@@ -17,32 +17,19 @@ export default function Store() {
   const { contract } = useContract(TOOLS_ADDRESS);
   const { data: nfts } = useNFTs(contract);
 
+  // 配置 Slick 的輪播設置
   const sliderSettings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 1, // 一次顯示一個 NFT
     slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 768, // For tablets and smaller devices
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480, // For mobile devices
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+    autoplay: true, // 自動播放
+    autoplaySpeed: 3000, // 每 3 秒切換
   };
 
   return (
-    <Container maxW={"1200px"}>
+    <Container maxW={"1200px"} mt={5}>
       {/* Header Section */}
       <Flex direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
         <Link href="/">
@@ -51,7 +38,7 @@ export default function Store() {
       </Flex>
 
       {/* Store Heading */}
-      <Heading mt={"40px"}>Store</Heading>
+      <Heading mt={5}>Store</Heading>
       <Text>Purchase tools with $CARROTS to increase your earnings.</Text>
 
       {/* NFT Carousel */}
