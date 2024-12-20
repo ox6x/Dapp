2f92bb1e25d760f0a4fdc522f5f78094c486ed56
acc0x <ox8xxx@gmail.com>
2024-12-21 00:18:33 +0700

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,129 +1,107 @@
-import {
-  ConnectWallet,
-  MediaRenderer,
-  useAddress,
-  useContract,
-  useContractRead,
-  useOwnedNFTs,
-} from "@thirdweb-dev/react";
+import { ConnectWallet, MediaRenderer, useAddress, useContract, useContractRead, useOwnedNFTs } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
-import { useRouter } from "next/router";
-import {
-  FARMER_ADDRESS,
-  REWARDS_ADDRESS,
-  STAKING_ADDRESS,
-  TOOLS_ADDRESS,
-} from "../const/addresses";
+import { FARMER_ADDRESS, REWARDS_ADDRESS, STAKING_ADDRESS, TOOLS_ADDRESS } from "../const/addresses";
 import { ClaimFarmer } from "../components/ClaimFarmer";
 import { Inventory } from "../components/Inventory";
 import { Equipped } from "../components/Equipped";
 import { BigNumber, ethers } from "ethers";
-import {
-  Text,
-  Box,
-  Flex,
-  Heading,
-  SimpleGrid,
-  Spinner,
-  Skeleton,
-} from "@chakra-ui/react";
+import { Text, Box, Card, Container, Flex, Heading, SimpleGrid, Spinner, Skeleton } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   const address = useAddress();
-  const router = useRouter();
 
-  const { contract: farmerContract } = useContract(FARMER_ADDRESS);
+  const { contract: farmercontract } = useContract(FARMER_ADDRESS);
   const { contract: toolsContract } = useContract(TOOLS_ADDRESS);
   const { contract: stakingContract } = useContract(STAKING_ADDRESS);
   const { contract: rewardContract } = useContract(REWARDS_ADDRESS);
 
-  const { data: ownedFarmers, isLoading: loadingOwnedFarmers } = useOwnedNFTs(
-    farmerContract,
-    address
-  );
-  const { data: ownedTools, isLoading: loadingOwnedTools } = useOwnedNFTs(
-    toolsContract,
-    address
-  );
+  const { data: ownedFarmers, isLoading: loadingOwnedFarmers } = useOwnedNFTs(farmercontract, address);
+  const { data: ownedTools, isLoading: loadingOwnedTools } = useOwnedNFTs(toolsContract, address);
 
-  const { data: equippedTools } = useContractRead(stakingContract, "getStakeInfo", [address]);
+  const { data: equippedTools } = useContractRead(
+    stakingContract, 
+    "getStakeInfo",
+    [address]
+  );
 
   const { data: rewardBalance } = useContractRead(rewardContract, "balanceOf", [address]);
-
-  // 如果用户未连接钱包，显示欢迎页
+  
   if (!address) {
     return (
-      <Flex direction="column" h="100vh" justifyContent="center" alignItems="center">
-        <Heading mb={6}>Welcome to Crypto Farm</Heading>
-        <ConnectWallet />
-      </Flex>
+      <Container maxW={"1200px"}>
+        <Flex direction={"column"} h={"100vh"} justifyContent={"center"} alignItems={"center"}>
+          <Heading my={"40px"}>Welcom to Crypto Farm</Heading>
+          <ConnectWallet />
+        </Flex>
+      </Container>
     );
   }
 
-  // 如果数据正在加载，显示 Spinner
   if (loadingOwnedFarmers) {
-    return (
-      <Flex h="100vh" justifyContent="center" alignItems="center">
-        <Spinner />
-      </Flex>
+    return(
+      <Container maxW={"1200px"}>
+        <Flex h={"100vh"} justifyContent={"center"} alignItems={"center"}>
+          <Spinner />
+        </Flex>
+      </Container>
     );
   }
 
-  // 如果用户没有 Farmers，显示领取 Farmer 的页面
   if (ownedFarmers?.length === 0) {
-    return <ClaimFarmer />;
+    return (
+      <Container maxW={"1200px"}>
+        <ClaimFarmer />
+      </Container>
+    );
   }
 
   return (
-    <Box px={4} py={6}>
+    <Container maxW={"1200px"}>
       <SimpleGrid columns={2} spacing={10}>
-        {/* 农夫信息 */}
-        <Box>
-          <Heading mb={4}>Farmer:</Heading>
-          <SimpleGrid columns={2} spacing={6}>
+        <Card p={5}>
+          <Heading>Farmer:</Heading>
+          <SimpleGrid columns={2} spacing={10}>
             <Box>
               {ownedFarmers?.map((nft) => (
-                <MediaRenderer
-                  key={nft.metadata.id}
-                  src={nft.metadata.image}
-                  height="100%"
-                  width="100%"
-                />
+                <div key={nft.metadata.id}>
+                  <MediaRenderer 
+                    src={nft.metadata.image} 
+                    height="100%"
+                    width="100%"
+                  />
+                </div>
               ))}
             </Box>
             <Box>
-              <Text fontSize="sm" fontWeight="bold">$CARROT Balance:</Text>
-              {rewardBalance && <Text>{ethers.utils.formatUnits(rewardBalance, 18)}</Text>}
-            </Box>
+              <Text fontSize={"small"} fontWeight={"bold"}>$CARROT Balance:</Text>
+                {rewardBalance && (
+                    <p>{ethers.utils.formatUnits(rewardBalance, 18)}</p>
+                  )}
+              </Box>
           </SimpleGrid>
-        </Box>
-
-        {/* Inventory，点击跳转到 Store */}
-        <Box>
-          <Heading
-            mb={4}
-            cursor="pointer"
-            onClick={() => router.push("/store")}
-          >
-            Inventory:
-          </Heading>
+        </Card>
+        <Card p={5}>
+          <Heading>Inventory:</Heading>
           <Skeleton isLoaded={!loadingOwnedTools}>
-            <Inventory nft={ownedTools} />
+            <Inventory
+              nft={ownedTools}
+            />     
           </Skeleton>
-        </Box>
+        </Card>
       </SimpleGrid>
-
-      {/* 装备的工具 */}
-      <Box mt={10}>
-        <Heading mb={6}>Equipped Tools:</Heading>
-        <SimpleGrid columns={3} spacing={6}>
-          {equippedTools &&
-            equippedTools[0].map((nft: BigNumber) => (
-              <Equipped key={nft.toNumber()} tokenId={nft.toNumber()} />
-            ))}
+      <Card p={5} my={10}>
+        <Heading mb={"30px"}>Equiped Tools:</Heading>
+        <SimpleGrid columns={3} spacing={10}>
+            {equippedTools &&
+              equippedTools[0].map((nft: BigNumber) => (
+                <Equipped
+                  key={nft.toNumber()}
+                  tokenId={nft.toNumber()}
+                />
+              ))}
         </SimpleGrid>
-      </Box>
-    </Box>
+      </Card>
+    </Container>
   );
 };
 
