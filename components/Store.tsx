45b658133d236e1bd5245d3eaf067bca34cce5a7
acc0x <ox8xxx@gmail.com>
2024-12-20 20:24:34 +0700

    Update Store.tsx

--- components/Store.tsx
+++ components/Store.tsx
@@ -1,41 +1,37 @@
 import { useContract, useNFTs } from "@thirdweb-dev/react";
+import { TOOLS_ADDRESS } from "../const/addresses";
 import Link from "next/link";
 import { Text, Button, Container, Flex, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
-import NFT from "./NFT";
+import NFT from "../components/NFT";
 
-interface StoreProps {
-  contractAddress: string; // 合约地址
-  title: string; // 标题
-  description: string; // 描述文本
-  backLink: string; // 返回按钮的链接
+export default function Store() {
+    const { contract } = useContract(TOOLS_ADDRESS);
+    const { data: nfts } = useNFTs(contract);
+    console.log(nfts);
+
+    return (
+        <Container maxW={"1200px"}>
+            <Flex direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
+                <Link href="/">
+                    <Button>Back</Button>
+                </Link>
+            </Flex>
+            <Heading mt={"40px"}>Store</Heading>
+            <Text>Purchase tools with $CARROTS to increase your earnings.</Text>
+            {!nfts ? (
+                <Flex h={"50vh"} justifyContent={"center"} alignItems={"center"}>
+                    <Spinner />
+                </Flex>
+            ) : (
+                <SimpleGrid columns={3} spacing={10}>
+                    {nfts?.map((nftItem) => (
+                        <NFT 
+                            key={nftItem.metadata.id}
+                            nft={nftItem}
+                        />
+                    ))}
+                </SimpleGrid>
+            )}
+        </Container>
+    );
 }
-
-const Store: React.FC<StoreProps> = ({ contractAddress, title, description, backLink }) => {
-  const { contract } = useContract(contractAddress);
-  const { data: nfts, isLoading } = useNFTs(contract);
-
-  return (
-    <Container maxW={"1200px"}>
-      <Flex direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
-        <Link href={backLink}>
-          <Button>Back</Button>
-        </Link>
-      </Flex>
-      <Heading mt={"40px"}>{title}</Heading>
-      <Text>{description}</Text>
-      {isLoading ? (
-        <Flex h={"50vh"} justifyContent={"center"} alignItems={"center"}>
-          <Spinner />
-        </Flex>
-      ) : (
-        <SimpleGrid columns={3} spacing={10}>
-          {nfts?.map((nftItem) => (
-            <NFT key={nftItem.metadata.id} nft={nftItem} />
-          ))}
-        </SimpleGrid>
-      )}
-    </Container>
-  );
-};
-
-export default Store;
