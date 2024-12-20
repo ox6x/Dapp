2992b1fb9fe2ecc33508558917947c9ea5f3e87b
acc0x <ox8xxx@gmail.com>
2024-12-20 19:25:22 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,38 +3,58 @@ import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { ChakraProvider, Box } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
-import Head from 'next/head';
+import Head from "next/head";
+import { useAddress } from "@thirdweb-dev/react";
+import { useEffect } from "react";
+import Login from "../components/Login";
 
 const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID as string;
+
+function AuthWrapper({ children }: { children: React.ReactNode }) {
+  const address = useAddress();
+
+  // 如果用户未登录，显示 Login 组件
+  if (!address) {
+    return (
+      <Box minH="100vh" display="flex" justifyContent="center" alignItems="center">
+        <Login />
+      </Box>
+    );
+  }
+
+  // 如果已登录，渲染实际页面内容
+  return <>{children}</>;
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
       clientId={CLIENT_ID}
       activeChain={{
-        chainId: 97,  // 修改为 97
-        rpc: ["https://97.rpc.thirdweb.com"],  // 修改为链 ID 97 对应的 RPC URL
+        chainId: 97,
+        rpc: ["https://97.rpc.thirdweb.com"],
         nativeCurrency: {
-          name: "Binance Smart Chain Testnet",  // 修改为 Testnet 的名称
-          symbol: "tBNB",  // 修改为 Testnet 的代币符号
+          name: "Binance Smart Chain Testnet",
+          symbol: "tBNB",
           decimals: 18,
         },
         name: "BSC Testnet",
         chain: "bsc-testnet",
         shortName: "tBNB",
-        testnet: true,  // Testnet 环境
-        slug: "bsc-testnet",  // 修改为 BSC Testnet 的 URL 友好名
+        testnet: true,
+        slug: "bsc-testnet",
       }}
     >
       <ChakraProvider>
         <Head>
-          {/* Prevent mobile scaling */}
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
-        <NavBar />
-        <Box minH="100vh" p={4}>
-          <Component {...pageProps} />
-        </Box>
+        <AuthWrapper>
+          <NavBar />
+          <Box minH="100vh" p={4}>
+            <Component {...pageProps} />
+          </Box>
+        </AuthWrapper>
       </ChakraProvider>
     </ThirdwebProvider>
   );
