c9a7324ae8e3b011cb17d41cf7715c1a0aa38e09
acc0x <ox8xxx@gmail.com>
2024-12-21 17:26:22 +0700

    Update store.tsx

--- pages/store.tsx
+++ pages/store.tsx
@@ -1,39 +1,53 @@
 import { useContract, useNFTs } from "@thirdweb-dev/react";
 import { TOOLS_ADDRESS } from "../const/addresses";
 import Link from "next/link";
-import { Text, Button, Container, Flex, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
+import { Text, Button, Container, Flex, Heading, Spinner } from "@chakra-ui/react";
 import NFT from "../components/NFT";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-export default function Store()  {
+export default function Store() {
     const { contract } = useContract(TOOLS_ADDRESS);
     const { data: nfts } = useNFTs(contract);
     console.log(nfts);
 
+    // Slick slider settings
+    const sliderSettings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: "0px",
+        autoplay: false,
+    };
+
     return (
         <Container maxW={"1200px"}>
             <Flex direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
-                <Link
-                    href="/"
-                >
+                <Link href="/">
                     <Button>Back</Button>
                 </Link>
             </Flex>
-            <Heading mt={"40px"}>Shop</Heading>
-            <Text>Purchase tools with $CARROTS to increase your earnings.</Text>
+            <Heading mt={"40px"} textAlign={"center"}>Shop</Heading>
+            <Text textAlign={"center"}>Purchase tools with $CARROTS to increase your earnings.</Text>
             {!nfts ? (
                 <Flex h={"50vh"} justifyContent={"center"} alignItems={"center"}>
                     <Spinner />
                 </Flex>
             ) : (
-                <SimpleGrid columns={3} spacing={10}>
-                    {nfts?.map((nftItem) => (
-                        <NFT 
-                            key={nftItem.metadata.id}
-                            nft={nftItem}
-                        />
-                    ))}
-                </SimpleGrid>
+                <Flex justifyContent={"center"} mt={10}>
+                    <Slider {...sliderSettings} style={{ width: "80%" }}>
+                        {nfts.map((nftItem) => (
+                            <Flex key={nftItem.metadata.id} justifyContent={"center"}>
+                                <NFT nft={nftItem} />
+                            </Flex>
+                        ))}
+                    </Slider>
+                </Flex>
             )}
         </Container>
-    )
+    );
 };
