14d63d0bab9d66439d793aea9a19d7a38e46e505
acc0x <ox8xxx@gmail.com>
2024-12-21 12:53:04 +0700

    Update Equipped.tsx

--- components/Equipped.tsx
+++ components/Equipped.tsx
@@ -2,14 +2,13 @@ import { MediaRenderer, Web3Button, useAddress, useContract, useContractRead, us
 import { STAKING_ADDRESS, TOOLS_ADDRESS } from "../const/addresses";
 import { ethers } from "ethers";
 import { Text, Box, Card, Stack, Flex } from "@chakra-ui/react";
-import { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 interface EquippedProps {
     tokenId: number;
-}
+};
 
 export const Equipped = (props: EquippedProps) => {
     const address = useAddress();
@@ -18,18 +17,12 @@ export const Equipped = (props: EquippedProps) => {
     const { data: nft } = useNFT(toolContract, props.tokenId);
 
     const { contract: stakingContract } = useContract(STAKING_ADDRESS);
+
     const { data: claimableRewards } = useContractRead(
         stakingContract,
         "getStakeInfoForToken",
         [props.tokenId, address]
     );
-
-    // State for quantity selection
-    const [quantity, setQuantity] = useState<number>(1);
-
-    const handleQuantityChange = (newQuantity: number) => {
-        setQuantity(Math.max(1, newQuantity)); // Ensure quantity is at least 1
-    };
 
     // Slider settings
     const sliderSettings = {
@@ -38,120 +31,44 @@ export const Equipped = (props: EquippedProps) => {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: true,
     };
 
     return (
         <Box>
-            {nft && claimableRewards && (
+            {nft && (
                 <Card p={5}>
-                    <Flex>
-                        {/* Slider for NFTs */}
-                        <Box
-                            maxWidth="200px"
-                            maxHeight="200px"
-                            overflow="hidden"
-                            borderRadius="10px"
-                            marginRight="20px"
-                        >
-                            <Slider {...sliderSettings}>
-                                {/* 主圖片 */}
-                                <div>
+                    <Slider {...sliderSettings}>
+                        <Box>
+                            <Flex>
+                                <Box>
                                     <MediaRenderer
                                         src={nft.metadata.image}
-                                        height="100%"
-                                        width="100%"
-                                        style={{ objectFit: "contain" }}
+                                        height="80%"
+                                        width="80%"
                                     />
-                                </div>
-                                {/* 額外圖片或動畫 (如果存在) */}
-                                {nft.metadata.animation_url && (
-                                    <div>
-                                        <MediaRenderer
-                                            src={nft.metadata.animation_url}
-                                            height="100%"
-                                            width="100%"
-                                            style={{ objectFit: "contain" }}
-                                        />
-                                    </div>
-                                )}
-                            </Slider>
+                                </Box>
+                                <Stack spacing={1}>
+                                    <Text fontSize={"2xl"} fontWeight={"bold"}>{nft.metadata.name}</Text>
+                                    <Text>Equipped: {ethers.utils.formatUnits(claimableRewards?.[0] || "0", 0)}</Text>
+                                    <Web3Button
+                                        contractAddress={STAKING_ADDRESS}
+                                        action={(contract) => contract.call("withdraw", [props.tokenId, 1])}
+                                    >Unequip</Web3Button>
+                                </Stack>
+                            </Flex>
                         </Box>
 
-                        {/* Details Section */}
-                        <Stack spacing={3}>
-                            <Text fontSize="2xl" fontWeight="bold">
-                                {nft.metadata.name}
-                            </Text>
-                            <Text>
-                                Equipped: {ethers.utils.formatUnits(claimableRewards[0] || 0, 0)}
-                            </Text>
-
-                            {/* Quantity Selector */}
-                            <Flex align="center" gap={2}>
-                                <button
-                                    onClick={() => handleQuantityChange(quantity - 1)}
-                                    style={{
-                                        padding: "5px 10px",
-                                        backgroundColor: "#f56565",
-                                        color: "#fff",
-                                        border: "none",
-                                        borderRadius: "5px",
-                                        cursor: "pointer",
-                                    }}
-                                >
-                                    -
-                                </button>
-                                <input
-                                    type="number"
-                                    value={quantity}
-                                    onChange={(e) => {
-                                        const value = parseInt(e.target.value);
-                                        handleQuantityChange(isNaN(value) ? 1 : value);
-                                    }}
-                                    style={{
-                                        width: "60px",
-                                        textAlign: "center",
-                                        border: "1px solid #ccc",
-                                        borderRadius: "5px",
-                                        padding: "5px",
-                                    }}
-                                />
-                                <button
-                                    onClick={() => handleQuantityChange(quantity + 1)}
-                                    style={{
-                                        padding: "5px 10px",
-                                        backgroundColor: "#48bb78",
-                                        color: "#fff",
-                                        border: "none",
-                                        borderRadius: "5px",
-                                        cursor: "pointer",
-                                    }}
-                                >
-                                    +
-                                </button>
-                            </Flex>
-
+                        <Box>
+                            <Text>Claimable $CARROT:</Text>
+                            <Text>{ethers.utils.formatUnits(claimableRewards?.[1] || "0", 18)}</Text>
                             <Web3Button
                                 contractAddress={STAKING_ADDRESS}
-                                action={(contract) => contract.call("withdraw", [props.tokenId, quantity])}
-                            >
-                                Unequip {quantity}
-                            </Web3Button>
-                        </Stack>
-                    </Flex>
-                    <Box mt={5}>
-                        <Text>Claimable $CARROT:</Text>
-                        <Text>{ethers.utils.formatUnits(claimableRewards[1] || 0, 18)}</Text>
-                        <Web3Button
-                            contractAddress={STAKING_ADDRESS}
-                            action={(contract) => contract.call("claimRewards", [props.tokenId])}
-                        >
-                            Claim $CARROT
-                        </Web3Button>
-                    </Box>
+                                action={(contract) => contract.call("claimRewards", [props.tokenId])}
+                            >Claim $CARROT</Web3Button>
+                        </Box>
+                    </Slider>
                 </Card>
             )}
         </Box>
-    );
+    )
 };
