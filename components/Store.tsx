89993ed3723502fddaec50ee932dbb31f8ecb6ca
acc0x <ox8xxx@gmail.com>
2024-12-21 16:28:38 +0700

    Update Store.tsx

--- components/Store.tsx
+++ components/Store.tsx
@@ -1,39 +1,36 @@
 import { useContract, useNFTs } from "@thirdweb-dev/react";
 import { TOOLS_ADDRESS } from "../const/addresses";
 import Link from "next/link";
-import { Text, Button, Container, Flex, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import NFT from "./NFT";
 
-export default function Store()  {
+export default function Store() {
     const { contract } = useContract(TOOLS_ADDRESS);
     const { data: nfts } = useNFTs(contract);
     console.log(nfts);
 
     return (
-        <Container maxW={"1200px"}>
-            <Flex direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
-                <Link
-                    href="/store"
-                >
-                    <Button>Back</Button>
+        <div>
+            <div>
+                <Link href="/store">
+                    <button>Back</button>
                 </Link>
-            </Flex>
-            <Heading mt={"40px"}>Store</Heading>
-            <Text>Purchase tools with $CARROTS to increase your earnings.</Text>
+            </div>
+            <h1>Store</h1>
+            <p>Purchase tools with $CARROTS to increase your earnings.</p>
             {!nfts ? (
-                <Flex h={"50vh"} justifyContent={"center"} alignItems={"center"}>
-                    <Spinner />
-                </Flex>
+                <div>
+                    <p>Loading...</p>
+                </div>
             ) : (
-                <SimpleGrid columns={3} spacing={10}>
+                <div>
                     {nfts?.map((nftItem) => (
                         <NFT 
                             key={nftItem.metadata.id}
                             nft={nftItem}
                         />
                     ))}
-                </SimpleGrid>
+                </div>
             )}
-        </Container>
-    )
-};
+        </div>
+    );
+}
