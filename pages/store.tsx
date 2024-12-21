d12a1aad735a61de75f69c0037c163c18fc5afc4
acc0x <ox8xxx@gmail.com>
2024-12-21 16:39:39 +0700

    Update store.tsx

--- pages/store.tsx
+++ pages/store.tsx
@@ -1,4 +1,39 @@
-// pages/store.tsx
-import Store from "../components/Store";
+import { useContract, useNFTs } from "@thirdweb-dev/react";
+import { TOOLS_ADDRESS } from "../const/addresses";
+import Link from "next/link";
+import { Text, Button, Container, Flex, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
+import NFT from "../components/NFT";
 
-export default Store;
+export default function Store()  {
+    const { contract } = useContract(TOOLS_ADDRESS);
+    const { data: nfts } = useNFTs(contract);
+    console.log(nfts);
+
+    return (
+        <Container maxW={"1200px"}>
+            <Flex direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
+                <Link
+                    href="/"
+                >
+                    <Button>Back</Button>
+                </Link>
+            </Flex>
+            <Heading mt={"40px"}>Shop</Heading>
+            <Text>Purchase tools with $CARROTS to increase your earnings.</Text>
+            {!nfts ? (
+                <Flex h={"50vh"} justifyContent={"center"} alignItems={"center"}>
+                    <Spinner />
+                </Flex>
+            ) : (
+                <SimpleGrid columns={3} spacing={10}>
+                    {nfts?.map((nftItem) => (
+                        <NFT 
+                            key={nftItem.metadata.id}
+                            nft={nftItem}
+                        />
+                    ))}
+                </SimpleGrid>
+            )}
+        </Container>
+    )
+};
