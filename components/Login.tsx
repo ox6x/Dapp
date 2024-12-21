0812e688ad3b4711ed8e649f02bf38b6ed00a664
acc0x <ox8xxx@gmail.com>
2024-12-21 22:51:40 +0700

    Update Login.tsx

--- components/Login.tsx
+++ components/Login.tsx
@@ -10,7 +10,17 @@ const Login = () => {
         {!address ? (
           <>
             <Heading my={"40px"}>Welcome to Crypto Farm</Heading>
-            <ConnectWallet />
+            <ConnectWallet
+              style={{
+                fontSize: "1rem", // 自适应字体大小
+                padding: "0.5rem 1rem", // 按钮大小
+              }}
+              modalSize={{
+                base: "100%", // 小屏幕自适应宽度
+                sm: "400px", // 中屏幕大小
+                md: "500px", // 大屏幕大小
+              }}
+            />
           </>
         ) : (
           <Heading my={"40px"}>Wallet Connected</Heading>
