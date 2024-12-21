bb0c86d7410d4fd82d5c38dd14bd0639ad65c9c0
acc0x <ox8xxx@gmail.com>
2024-12-21 15:39:58 +0700

    Add files via upload

--- components/Inventory.tsx
+++ components/Inventory.tsx
@@ -1,9 +1,8 @@
-import { useState } from 'react';
 import { MediaRenderer, Web3Button, useAddress, useContract } from '@thirdweb-dev/react';
 import { NFT } from '@thirdweb-dev/sdk';
 import { STAKING_ADDRESS, TOOLS_ADDRESS } from '../const/addresses';
+import Link from 'next/link';
 import { Text, Box, Button, Card, SimpleGrid, Stack } from '@chakra-ui/react';
-import Store from './Store'; // 引入 Store 组件
 
 type Props = {
     nft: NFT[] | undefined;
@@ -13,9 +12,6 @@ export function Inventory({ nft }: Props) {
     const address = useAddress();
     const { contract: toolContract } = useContract(TOOLS_ADDRESS);
     const { contract: stakingContract } = useContract(STAKING_ADDRESS);
-
-    // 用于控制是否显示商店
-    const [showStore, setShowStore] = useState(false);
 
     async function stakeNFT(id: string) {
         if (!address) {
@@ -36,58 +32,37 @@ export function Inventory({ nft }: Props) {
         await stakingContract?.call("stake", [id, 1]);
     };
 
-    if (nft?.length === 0) {
+    if(nft?.length === 0) {
         return (
-            <Box textAlign="center" mt={6}>
-                <Text fontSize="lg" mb={4}>No tools available.</Text>
-                {!showStore ? (
-                    <Button colorScheme="teal" onClick={() => setShowStore(true)}>
-                        Shop Tool
-                    </Button>
-                ) : (
-                    <Box mt={6}>
-                        <Store />
-                    </Box>
-                )}
+            <Box>
+                <Text>No tools.</Text>
+                <Link
+                    href="/shop"
+                >
+                    <Button>Shop Tool</Button>
+                </Link>
             </Box>
-        );
+        )
     }
 
     return (
-        <Box>
-            <SimpleGrid columns={3} spacing={4}>
-                {nft?.map((nft) => (
-                    <Card key={nft.metadata.id} p={5}>
-                        <Stack alignItems={"center"}>
-                            <MediaRenderer 
-                                src={nft.metadata.image} 
-                                height="100px"
-                                width="100px"
-                            />
-                            <Text>{nft.metadata.name}</Text>
-                            <Web3Button
-                                contractAddress={STAKING_ADDRESS}
-                                action={() => stakeNFT(nft.metadata.id)}
-                            >
-                                Equip
-                            </Web3Button>
-                        </Stack>
-                    </Card>
-                ))}
-            </SimpleGrid>
-
-            {/* 显示商店内容按钮 */}
-            <Box mt={6} textAlign="center">
-                {!showStore ? (
-                    <Button colorScheme="teal" onClick={() => setShowStore(true)}>
-                        Shop More Tools
-                    </Button>
-                ) : (
-                    <Box mt={6}>
-                        <Store />
-                    </Box>
-                )}
-            </Box>
-        </Box>
+        <SimpleGrid columns={3} spacing={4}>
+            {nft?.map((nft) => (
+                <Card key={nft.metadata.id} p={5}>
+                    <Stack alignItems={"center"}>
+                    <MediaRenderer 
+                        src={nft.metadata.image} 
+                        height="100px"
+                        width="100px"
+                    />
+                    <Text>{nft.metadata.name}</Text>
+                    <Web3Button
+                        contractAddress={STAKING_ADDRESS}
+                        action={() => stakeNFT(nft.metadata.id)}
+                    >Equip</Web3Button>
+                    </Stack>
+                </Card>
+            ))}
+        </SimpleGrid>
     );
 };
