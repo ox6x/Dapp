05639c4325756a45758102a76a40a27fae911d06
acc0x <ox8xxx@gmail.com>
2024-12-21 22:50:13 +0700

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,15 +1,15 @@
-import { Container, Flex, Heading, Link } from "@chakra-ui/react";
+import { Container, Flex, Heading } from "@chakra-ui/react";
 import { ConnectWallet } from "@thirdweb-dev/react";
 
 export default function NavBar() {
     return (
         <Container maxW={"1200px"} py={4}>
-            <Flex direction={"row"} justifyContent={"space-between"}>
+            <Flex direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
                 <Heading>Crypto Farm</Heading>
-                <Flex alignItems={"center"}>
+                <Flex alignItems={"center"} justifyContent={"flex-end"} w="auto">
+                    <ConnectWallet style={{ fontSize: "1rem", padding: "0.5rem 1rem" }} />
                 </Flex>
-                <ConnectWallet/>
             </Flex>
         </Container>
-    )
-};
+    );
+}
