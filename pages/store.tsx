e7d576c06e0fb2c1e55211342508ad5e21b93843
acc0x <ox8xxx@gmail.com>
2024-12-22 13:34:09 +0700

    Add files via upload

--- pages/store.tsx
+++ pages/store.tsx
@@ -4,85 +4,48 @@ import "slick-carousel/slick/slick-theme.css";
 import { useContract, useNFTs } from "@thirdweb-dev/react";
 import { TOOLS_ADDRESS } from "../const/addresses";
 import Link from "next/link";
-import {
-  Text,
-  Button,
-  Container,
-  Flex,
-  Heading,
-  Spinner,
-  Stack,
-} from "@chakra-ui/react";
-import NFTQuantityTransaction from "../components/NFTQuantityTransaction";
+import { Text, Button, Container, Flex, Heading, Spinner } from "@chakra-ui/react";
+import NFT from "../components/NFT";
 
 export default function Store() {
-  const { contract } = useContract(TOOLS_ADDRESS);
-  const { data: nfts, isLoading } = useNFTs(contract);
+    const { contract } = useContract(TOOLS_ADDRESS);
+    const { data: nfts } = useNFTs(contract);
+    console.log(nfts);
 
-  const pricePerItem = 10; // 單個 NFT 的靜態價格，可根據需求改為動態值
+    const sliderSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1, // 一次只顯示一個
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        centerMode: true, // 啟用置中
+        centerPadding: "0", // 確保內容完全置中
+    };
 
-  const sliderSettings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1, // 一次只顯示一個
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    centerMode: true, // 啟用置中
-    centerPadding: "0", // 確保內容完全置中
-  };
-
-  // 如果載入中，顯示 Spinner
-  if (isLoading) {
     return (
-      <Flex h={"50vh"} justifyContent={"center"} alignItems={"center"}>
-        <Spinner />
-      </Flex>
+        <Container maxW={"1200px"}>
+            <Flex direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
+                <Link href="/">
+                    <Button>Back</Button>
+                </Link>
+            </Flex>
+            <Heading mt={"40px"}>Store</Heading>
+            <Text>Purchase tools with $CARROTS to increase your earnings.</Text>
+            {!nfts ? (
+                <Flex h={"50vh"} justifyContent={"center"} alignItems={"center"}>
+                    <Spinner />
+                </Flex>
+            ) : (
+                <Slider {...sliderSettings}>
+                    {nfts?.map((nftItem) => (
+                        <div key={nftItem.metadata.id}>
+                            <NFT nft={nftItem} />
+                        </div>
+                    ))}
+                </Slider>
+            )}
+        </Container>
     );
-  }
-
-  return (
-    <Container maxW={"1200px"}>
-      {/* 頁面頂部的返回按鈕 */}
-      <Flex direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
-        <Link href="/">
-          <Button>Back</Button>
-        </Link>
-      </Flex>
-
-      <Heading mt={"40px"}>Store</Heading>
-      <Text>Purchase tools with $CARROTS to increase your earnings.</Text>
-
-      {/* 如果 nfts 有資料，使用 Slider；否則給予空狀態提示 */}
-      {!nfts || nfts.length === 0 ? (
-        <Text mt={8}>No NFTs available in the store at the moment.</Text>
-      ) : (
-        <Slider {...sliderSettings}>
-          {nfts.map((nftItem) => (
-            <Stack key={nftItem.metadata.id} p={5} spacing={4} align={"center"}>
-              <img
-                src={nftItem.metadata.image}
-                alt={nftItem.metadata.name}
-                width="150px"
-              />
-              <Text fontWeight={"bold"}>{nftItem.metadata.name}</Text>
-
-              {/* 數量選擇器與購買按鈕 */}
-              <NFTQuantityTransaction
-                initialQuantity={1}
-                minQuantity={1}
-                getPrice={(quantity) => `${quantity * pricePerItem} CARROTS`} // 動態價格計算
-                onTransaction={(quantity) => {
-                  alert(`Purchasing ${quantity} items for ${quantity * pricePerItem} CARROTS`);
-                }}
-                onTransactionConfirmed={() => alert("Purchase confirmed!")}
-                buttonText="BUY"
-              />
-            </Stack>
-          ))}
-        </Slider>
-      )}
-    </Container>
-  );
 }
