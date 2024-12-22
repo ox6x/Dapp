de37af0a217bf203ae3bc49f2c4376c3bf686e05
acc0x <ox8xxx@gmail.com>
2024-12-22 07:33:26 +0700

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,12 +1,19 @@
-import { Container, Flex, Heading } from "@chakra-ui/react";
+import { Container, Flex, Heading, Link } from "@chakra-ui/react";
 import { ConnectWallet } from "@thirdweb-dev/react";
 
 export default function NavBar() {
     return (
         <Container maxW={"1200px"} py={4}>
             <Flex direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
-                <Heading>Crypto Farm</Heading>
+                <Heading>
+                    <Link href="/" style={{ textDecoration: "none" }}>
+                        Crypto Farm
+                    </Link>
+                </Heading>
                 <Flex alignItems={"center"} justifyContent={"flex-end"} w="auto">
+                    <Link href="/store" style={{ marginRight: "1rem", fontSize: "1rem", textDecoration: "none" }}>
+                        Store
+                    </Link>
                     <ConnectWallet style={{ fontSize: "1rem", padding: "0.5rem 1rem" }} />
                 </Flex>
             </Flex>
