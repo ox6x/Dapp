8ba04373778b6eceae94fe9aaf62644a196a590a
acc0x <ox8xxx@gmail.com>
2024-12-22 20:20:04 +0700

    Update ClaimFarmer.tsx

--- components/ClaimFarmer.tsx
+++ components/ClaimFarmer.tsx
@@ -1,28 +1,28 @@
 import { MediaRenderer, Web3Button, useContract, useContractMetadata } from "@thirdweb-dev/react";
 import { FARMER_ADDRESS } from "../const/addresses";
-import { Box, Container, Flex, Heading } from "@chakra-ui/react";
+import styles from "./ClaimFarmer.module.scss"; // 引入 SCSS 文件
 
 export function ClaimFarmer() {
     const { contract } = useContract(FARMER_ADDRESS);
     const { data: metadata } = useContractMetadata(contract);
     
     return (
-        <Container maxW={"1200px"}>
-            <Flex direction={"column"} alignItems={"center"} justifyContent={"center"} h={"50vh"}>
-                <Heading>Claim Farmer to start farming</Heading>
-                <Box borderRadius={"8px"} overflow={"hidden"} my={10}>
+        <div className={styles.container}>
+            <div className={styles.flex}>
+                <h1 className={styles.heading}>Claim Farmer to start farming</h1>
+                <div className={styles.box}>
                     <MediaRenderer
                         src={metadata?.image}
                         height="300px"
                         width="300px"
                     />
-                </Box>
+                </div>
                 
                 <Web3Button
                     contractAddress={FARMER_ADDRESS}
                     action={(contract) => contract.erc1155.claim(0, 1)}
                 >Claim Farmer</Web3Button>
-            </Flex>
-        </Container>
+            </div>
+        </div>
     );
 }
