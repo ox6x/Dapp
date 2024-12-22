eb2a1344506a3d562310e4ee34a373b7da5425ce
acc0x <ox8xxx@gmail.com>
2024-12-23 00:46:45 +0700

    Update Farmer.tsx

--- components/Farmer.tsx
+++ components/Farmer.tsx
@@ -1,42 +1,35 @@
+import { MediaRenderer } from "@thirdweb-dev/react";
 import { Box, Card, Heading, Text } from "@chakra-ui/react";
-import { ethers } from "ethers";
+import { BigNumber, ethers } from "ethers";
 
-type FarmerProps = {
-  farmers: any[];
-  rewardBalance: any;
-};
+interface FarmerProps {
+  ownedFarmers: any[];
+  rewardBalance: BigNumber | null;
+}
 
-const Farmer: React.FC<FarmerProps> = ({ farmers, rewardBalance }) => {
+export const Farmer = ({ ownedFarmers, rewardBalance }: FarmerProps) => {
   return (
-    <Card p={4}>
-      <Heading fontSize="lg" mb={4}>
-        Farmer
-      </Heading>
-      {farmers?.map((nft) => (
-        <Box
-          key={nft.metadata.id}
-          borderWidth="1px"
-          borderRadius="lg"
-          overflow="hidden"
-          mb={4}
-        >
-          <img
-            src={nft.metadata.image}
-            alt={nft.metadata.name}
-            style={{ height: "150px", width: "100%", objectFit: "cover" }}
-          />
-        </Box>
-      ))}
-      <Text fontSize={"sm"} fontWeight={"bold"} mb={2}>
-        bBNB Balance:
-      </Text>
-      {rewardBalance && (
-        <Text fontSize={"sm"}>
-          {ethers.utils.formatUnits(rewardBalance, 18)}
+    <Box mb={6}>
+      <Card p={4}>
+        <Heading fontSize="lg" mb={4}>
+          Farmer
+        </Heading>
+        {ownedFarmers?.map((nft) => (
+          <Box key={nft.metadata.id} borderWidth="1px" borderRadius="lg" overflow="hidden" mb={4}>
+            <MediaRenderer
+              src={nft.metadata.image}
+              height="150px"
+              width="100%"
+            />
+          </Box>
+        ))}
+        <Text fontSize={"sm"} fontWeight={"bold"} mb={2}>
+          bBNB Balance:
         </Text>
-      )}
-    </Card>
+        {rewardBalance && (
+          <Text fontSize={"sm"}>{ethers.utils.formatUnits(rewardBalance, 18)}</Text>
+        )}
+      </Card>
+    </Box>
   );
 };
-
-export default Farmer;
