81cdea60cf15c7854d5175b2d554b07a868ced87
acc0x <ox8xxx@gmail.com>
2024-12-22 07:09:36 +0700

    Update ToolsInventory.tsx

--- components/ToolsInventory.tsx
+++ components/ToolsInventory.tsx
@@ -1,20 +1,20 @@
-import { Box, Card, Heading, Skeleton } from "@chakra-ui/react";
-import { Inventory } from "./Inventory";
+import { Box, Card, Heading } from "@chakra-ui/react";
+import { BigNumber } from "ethers";
+import { Equipped } from "./Equipped";
 
-interface ToolsInventoryProps {
-  ownedTools: any[]; // 工具 NFT 数据
-  loadingOwnedTools: boolean; // 工具加载状态
+interface EquippedToolsProps {
+  equippedTools: any[]; // 已装备的工具数据
 }
 
-export const ToolsInventory = ({ ownedTools, loadingOwnedTools }: ToolsInventoryProps) => (
-  <Box mb={6}>
+export const EquippedTools = ({ equippedTools }: EquippedToolsProps) => (
+  <Box>
     <Card p={4}>
       <Heading fontSize="lg" mb={4}>
-        Inventory
+        Equipped Tools
       </Heading>
-      <Skeleton isLoaded={!loadingOwnedTools}>
-        {ownedTools.length > 0 ? <Inventory nft={ownedTools} /> : "No tools available"}
-      </Skeleton>
+      {equippedTools?.[0]?.map((nft: BigNumber) => (
+        <Equipped key={nft.toNumber()} tokenId={nft.toNumber()} />
+      )) || "No equipped tools"}
     </Card>
   </Box>
 );
