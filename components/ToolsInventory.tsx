13ad5234ecbb94de0b47007ceb2e8a008e7f69f3
acc0x <ox8xxx@gmail.com>
2024-12-22 07:03:23 +0700

    Create ToolsInventory.tsx

---
+++ components/ToolsInventory.tsx
@@ -0,0 +1,20 @@
+import { Box, Card, Heading, Skeleton } from "@chakra-ui/react";
+import { Inventory } from "./Inventory";
+
+interface ToolsInventoryProps {
+  ownedTools: any[]; // 工具 NFT 数据
+  loadingOwnedTools: boolean; // 工具加载状态
+}
+
+export const ToolsInventory = ({ ownedTools, loadingOwnedTools }: ToolsInventoryProps) => (
+  <Box mb={6}>
+    <Card p={4}>
+      <Heading fontSize="lg" mb={4}>
+        Inventory
+      </Heading>
+      <Skeleton isLoaded={!loadingOwnedTools}>
+        <Inventory nft={ownedTools} />
+      </Skeleton>
+    </Card>
+  </Box>
+);
