c00197db5d2ccff0aa1870d2e4cee723b9599e65
acc0x <ox8xxx@gmail.com>
2024-12-23 01:01:30 +0700

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,14 @@
 import { Container, Flex, Heading, Link } from "@chakra-ui/react";
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
 
 export default function NavBar() {
+    const address = useAddress(); // 获取钱包地址
+
+    // 如果未连接钱包，不渲染 NavBar
+    if (!address) {
+        return null;
+    }
+
     return (
         <Container maxW={"1200px"} py={4}>
             <Flex direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
