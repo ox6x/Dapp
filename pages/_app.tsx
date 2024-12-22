7be478a133f0bca5f045fc0b16ee692da9efaf0d
acc0x <ox8xxx@gmail.com>
2024-12-23 00:15:38 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,10 @@
 /** @jsxImportSource @emotion/react */
-import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { ChakraProvider, Box } from "@chakra-ui/react";
-import NavBar from "../components/NavBar";
-import Head from "next/head";
-import { useAddress } from "@thirdweb-dev/react";
-import { useEffect } from "react";
-import Login from "../components/Login";
 
 const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID as string;
 
-function AuthWrapper({ children }: { children: React.ReactNode }) {
-  const address = useAddress();
-
-  // 如果用户未登录，显示 Login 组件
-  if (!address) {
-    return (
-      <Box minH="100vh" display="flex" justifyContent="center" alignItems="center">
-        <Login />
-      </Box>
-    );
-  }
-
-  // 如果已登录，渲染实际页面内容
-  return <>{children}</>;
-}
-
-function MyApp({ Component, pageProps }: AppProps) {
+export default function Home() {
   return (
     <ThirdwebProvider
       clientId={CLIENT_ID}
@@ -46,18 +24,10 @@ function MyApp({ Component, pageProps }: AppProps) {
       }}
     >
       <ChakraProvider>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Head>
-        <AuthWrapper>
-          <NavBar />
-          <Box minH="100vh" p={4}>
-            <Component {...pageProps} />
-          </Box>
-        </AuthWrapper>
+        <Box minH="100vh" display="flex" alignItems="center" justifyContent="center">
+          <h1>Welcome to MyApp</h1>
+        </Box>
       </ChakraProvider>
     </ThirdwebProvider>
   );
 }
-
-export default MyApp;
