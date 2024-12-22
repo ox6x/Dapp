8248f01b436012c9a5794d4c1e3feb2f1ed83eb8
acc0x <ox8xxx@gmail.com>
2024-12-23 01:39:56 +0700

    Update Equipped.tsx

--- components/Equipped.tsx
+++ components/Equipped.tsx
@@ -1,83 +1,31 @@
-
-import { MediaRenderer, Web3Button, useAddress, useContract, useContractRead, useNFT } from "@thirdweb-dev/react";
-import { STAKING_ADDRESS, TOOLS_ADDRESS } from "../const/addresses";
-import { ethers } from "ethers";
-import { Text, Box, Card, Stack, Flex } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Text, Box, Stack, Card } from "@chakra-ui/react";
+import Quantity from "./Quantity";
 
 interface EquippedProps {
-    tokenId: number;
-};
+  tokenId: number;
+}
 
-export const Equipped = (props: EquippedProps) => {
-    const address = useAddress();
+const Equipped: React.FC<EquippedProps> = ({ tokenId }) => {
+  const [status, setStatus] = useState("Off");
 
-    const { contract: toolContract } = useContract(TOOLS_ADDRESS);
-    const { data: nft } = useNFT(toolContract, props.tokenId);
-
-    const { contract: stakingContract } = useContract(STAKING_ADDRESS);
-
-    const { data: claimableRewards } = useContractRead(
-        stakingContract,
-        "getStakeInfoForToken",
-        [props.tokenId, address]
-    );
-
-    return (
-        <Box>
-            {nft && (
-                <Card p={5}>
-                    <Flex>
-                        <Box>
-                            <MediaRenderer
-                                src={nft.metadata.image}
-                                height="80%"
-                                width="80%"
-                            />
-                        </Box>
-                        <Stack spacing={1}>
-                            <Text fontSize={"2xl"} fontWeight={"bold"}>{nft.metadata.name}</Text>
-                            <Text>Equipped: {ethers.utils.formatUnits(claimableRewards[0], 0)}</Text>
-                            <Web3Button
-                                contractAddress={STAKING_ADDRESS}
-                                action={(contract) => contract.call("withdraw", [props.tokenId, 1])}
-                            >Unequip</Web3Button>
-                        </Stack>
-                    </Flex>
-                    <Box mt={5}>
-                        <Text>Claimable $CARROT:</Text>
-                        <Text>{ethers.utils.formatUnits(claimableRewards[1], 18)}</Text>
-                        <Web3Button
-                            contractAddress={STAKING_ADDRESS}
-                            action={(contract) => contract.call("claimRewards", [props.tokenId])}
-                        >Claim $CARROT</Web3Button>
-                    </Box>
-                </Card>
-            )}
-        </Box>
-    )
-};
-
-import { useState } from "react";
-import { Quantity } from "./Quantity"; // Importing the Quantity component
-
-export const Equipped = (props) => {
-  const [status, setStatus] = useState("Off"); // Default status as Off
-
-  const handleStatusChange = (quantity) => {
+  const handleQuantityChange = (quantity: number) => {
     setStatus(quantity === 1 ? "On" : "Off");
-    console.log(`Status changed to: ${quantity === 1 ? "On" : "Off"}`);
   };
 
   return (
-    <div>
-      <h1>Equipped Component</h1>
-      <p>Current Status: {status}</p>
-
-      <Quantity
-        onQuantityChange={handleStatusChange}
-        minQuantity={0}
-        buttonText={status === "Off" ? "Turn On" : "Turn Off"}
-      />
-    </div>
+    <Card>
+      <Stack spacing={4}>
+        <Text>Token ID: {tokenId}</Text>
+        <Text>Status: {status}</Text>
+        <Quantity
+          onQuantityChange={handleQuantityChange}
+          minQuantity={0}
+          buttonText={status === "Off" ? "Activate" : "Deactivate"}
+        />
+      </Stack>
+    </Card>
   );
 };
+
+export default Equipped;
