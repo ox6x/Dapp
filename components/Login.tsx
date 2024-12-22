528ad8cb291b61b68432f24bbea69d4c09866ce7
acc0x <ox8xxx@gmail.com>
2024-12-23 00:33:42 +0700

    Update Login.tsx

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,26 +1,21 @@
 import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
-import { Flex, Heading, Container } from "@chakra-ui/react";
+import { Container, Flex, Heading } from "@chakra-ui/react";
 
-const Login = () => {
+const Login: React.FC = () => {
   const address = useAddress();
 
+  if (address) return null; // 如果已連結錢包，返回 null（或重定向）。
+
   return (
-    <Container maxW={"1200px"}>
+    <Container maxW={"container.sm"} px={4}>
       <Flex direction={"column"} h={"100vh"} justifyContent={"center"} alignItems={"center"}>
-        {!address ? (
-          <>
-            <Heading my={"40px"}>Welcome to Crypto Farm</Heading>
-            <ConnectWallet />
-          </>
-        ) : (
-          <Heading my={"40px"}>Wallet Connected</Heading>
-        )}
+        <Heading my={6} textAlign="center" fontSize="2xl">
+          Welcome to Crypto Farm
+        </Heading>
+        <ConnectWallet />
       </Flex>
     </Container>
   );
 };
-
-// 不使用佈局
-Login.getLayout = (page: React.ReactNode) => <>{page}</>;
 
 export default Login;
