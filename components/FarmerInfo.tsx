84de0528d9e94c15c396cb13952c5304df33eb65
acc0x <ox8xxx@gmail.com>
2024-12-22 07:02:55 +0700

    Create FarmerInfo.tsx

---
+++ components/FarmerInfo.tsx
@@ -0,0 +1,33 @@
+import { Box, Card, Heading, Text } from "@chakra-ui/react";
+import { MediaRenderer } from "@thirdweb-dev/react";
+import { ethers } from "ethers";
+
+interface FarmerInfoProps {
+  ownedFarmers: any[]; // Farmer NFT 数据
+  rewardBalance: any;  // 奖励余额
+}
+
+export const FarmerInfo = ({ ownedFarmers, rewardBalance }: FarmerInfoProps) => (
+  <Box mb={6}>
+    <Card p={4}>
+      <Heading fontSize="lg" mb={4}>
+        Farmer
+      </Heading>
+      {ownedFarmers?.map((nft) => (
+        <Box key={nft.metadata.id} borderWidth="1px" borderRadius="lg" overflow="hidden" mb={4}>
+          <MediaRenderer 
+            src={nft.metadata.image} 
+            height="150px"
+            width="100%"
+          />
+        </Box>
+      ))}
+      <Text fontSize={"sm"} fontWeight={"bold"} mb={2}>
+        $CARROT Balance:
+      </Text>
+      {rewardBalance && (
+        <Text fontSize={"sm"}>{ethers.utils.formatUnits(rewardBalance, 18)}</Text>
+      )}
+    </Card>
+  </Box>
+);
