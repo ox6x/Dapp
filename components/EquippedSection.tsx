4322186ca9812ee191a7b2633fd5fcfb0ea55dfc
acc0x <ox8xxx@gmail.com>
2024-12-23 00:53:41 +0700

    Create EquippedSection.tsx

---
+++ components/EquippedSection.tsx
@@ -0,0 +1,19 @@
+import { Box, Card, Heading } from "@chakra-ui/react";
+import { Equipped } from "./Equipped";
+import { BigNumber } from "ethers";
+
+const EquippedSection = ({ equippedTools }: any) => (
+  <Box>
+    <Card p={4}>
+      <Heading fontSize="lg" mb={4}>
+        Equipped
+      </Heading>
+      {equippedTools &&
+        equippedTools[0].map((nft: BigNumber) => (
+          <Equipped key={nft.toNumber()} tokenId={nft.toNumber()} />
+        ))}
+    </Card>
+  </Box>
+);
+
+export default EquippedSection;
