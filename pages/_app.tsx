08d1dd77777c8eb05ee63c58a6b5f0603b7b10b7
acc0x <ox8xxx@gmail.com>
2024-12-22 19:06:29 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,31 @@
 /** @jsxImportSource @emotion/react */
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import { ChakraProvider, Box } from "@chakra-ui/react";
+import { ThirdwebProvider, useAddress, useContract, useOwnedNFTs } from "@thirdweb-dev/react";
+import { ChakraProvider, Box, Container, Flex, Heading } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
 import Head from "next/head";
-import { useAddress } from "@thirdweb-dev/react";
-import { useEffect } from "react";
 import Login from "../components/Login";
+import { ClaimFarmer } from "../components/ClaimFarmer";
+import { FARMER_ADDRESS } from "../const/addresses";
+import { useEffect } from "react";
 
 const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID as string;
 
 function AuthWrapper({ children }: { children: React.ReactNode }) {
   const address = useAddress();
+  const { contract: farmerContract } = useContract(FARMER_ADDRESS);
+  const { data: ownedFarmers, isLoading } = useOwnedNFTs(farmerContract, address);
+
+  // 加载状态时显示 Loading
+  if (isLoading) {
+    return (
+      <Container maxW={"container.sm"} px={4}>
+        <Flex h={"100vh"} justifyContent={"center"} alignItems={"center"}>
+          <Heading size="lg">Loading...</Heading>
+        </Flex>
+      </Container>
+    );
+  }
 
   // 如果用户未登录，显示 Login 组件
   if (!address) {
@@ -22,7 +36,16 @@ function AuthWrapper({ children }: { children: React.ReactNode }) {
     );
   }
 
-  // 如果已登录，渲染实际页面内容
+  // 如果已登录但没有通行证，显示 ClaimFarmer
+  if (ownedFarmers?.length === 0) {
+    return (
+      <Box minH="100vh" display="flex" justifyContent="center" alignItems="center">
+        <ClaimFarmer />
+      </Box>
+    );
+  }
+
+  // 如果已登录且有通行证，渲染实际页面内容
   return <>{children}</>;
 }
 
