3ee0ceb383188d7bf6511d0837f50ab9bdf230a9
acc0x <ox8xxx@gmail.com>
2024-12-22 07:10:03 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,21 @@
 import { ConnectWallet, useAddress, useContract, useContractRead, useOwnedNFTs } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
-import { FARMER_ADDRESS, REWARDS_ADDRESS, STAKING_ADDRESS, TOOLS_ADDRESS } from "../const/addresses";
 import { FarmerInfo } from "../components/FarmerInfo";
 import { ToolsInventory } from "../components/ToolsInventory";
 import { EquippedTools } from "../components/EquippedTools";
 import { ClaimFarmer } from "../components/ClaimFarmer";
 import { Container, Flex, Heading, Spinner } from "@chakra-ui/react";
+import { FARMER_ADDRESS, REWARDS_ADDRESS, STAKING_ADDRESS, TOOLS_ADDRESS } from "../const/addresses";
 
 const Home: NextPage = () => {
   const address = useAddress();
 
-  const { contract: farmercontract } = useContract(FARMER_ADDRESS);
+  const { contract: farmerContract } = useContract(FARMER_ADDRESS);
   const { contract: toolsContract } = useContract(TOOLS_ADDRESS);
   const { contract: stakingContract } = useContract(STAKING_ADDRESS);
   const { contract: rewardContract } = useContract(REWARDS_ADDRESS);
 
-  const { data: ownedFarmers, isLoading: loadingOwnedFarmers } = useOwnedNFTs(farmercontract, address);
+  const { data: ownedFarmers, isLoading: loadingOwnedFarmers } = useOwnedNFTs(farmerContract, address);
   const { data: ownedTools, isLoading: loadingOwnedTools } = useOwnedNFTs(toolsContract, address);
   const { data: equippedTools } = useContractRead(stakingContract, "getStakeInfo", [address]);
   const { data: rewardBalance } = useContractRead(rewardContract, "balanceOf", [address]);
@@ -53,9 +53,9 @@ const Home: NextPage = () => {
 
   return (
     <Container maxW={"container.sm"} px={4} py={6}>
-      <FarmerInfo ownedFarmers={ownedFarmers} rewardBalance={rewardBalance} />
-      <ToolsInventory ownedTools={ownedTools} loadingOwnedTools={loadingOwnedTools} />
-      <EquippedTools equippedTools={equippedTools} />
+      <FarmerInfo ownedFarmers={ownedFarmers || []} rewardBalance={rewardBalance} />
+      <ToolsInventory ownedTools={ownedTools || []} loadingOwnedTools={loadingOwnedTools} />
+      <EquippedTools equippedTools={equippedTools || []} />
     </Container>
   );
 };
