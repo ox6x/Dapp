272b2272c4248755f5ac71936e1de8b74e9f5d23
acc0x <ox8xxx@gmail.com>
2024-12-23 00:52:50 +0700

    Create InventorySection.tsx

---
+++ components/InventorySection.tsx
@@ -0,0 +1,17 @@
+import { Box, Card, Heading, Skeleton } from "@chakra-ui/react";
+import { Inventory } from "../components/Inventory";
+
+const InventorySection = ({ ownedTools, loadingOwnedTools }: any) => (
+  <Box mb={6}>
+    <Card p={4}>
+      <Heading fontSize="lg" mb={4}>
+        Inventory
+      </Heading>
+      <Skeleton isLoaded={!loadingOwnedTools}>
+        <Inventory nft={ownedTools} />
+      </Skeleton>
+    </Card>
+  </Box>
+);
+
+export default InventorySection;
