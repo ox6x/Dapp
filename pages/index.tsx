cd87312b33a95e5d8ee7d96ade6d9ad3aa7d9828
ox6x <ox6.x@proton.me>
2024-12-24 01:40:21 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import { useAddress, useContract, useContractRead, useOwnedNFTs } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import { ADDRESSES } from "../const/addresses";
 import { ClaimFarmer } from "../components/ClaimFarmer";
-import { Container } from "@chakra-ui/react";
+import { Container, Button, Select } from "@chakra-ui/react";
 
 import LoginSection from "../components/LoginSection";
 import LoadingScreen from "../components/LoadingScreen";
@@ -12,11 +13,16 @@ import EquippedSection from "../components/EquippedSection";
 
 const Home: NextPage = () => {
   const address = useAddress();
+  const [contractIndex, setContractIndex] = useState(0);
+
+  const handleSwitchContract = (index: number) => {
+    setContractIndex(index);
+  };
 
   const { contract: farmerContract } = useContract(ADDRESSES.FARMER);
-  const { contract: toolsContract } = useContract(ADDRESSES.TOOLS_0); // 根據需求更改索引
-  const { contract: stakingContract } = useContract(ADDRESSES.STAKING_0);
-  const { contract: rewardContract } = useContract(ADDRESSES.REWARDS_0);
+  const { contract: toolsContract } = useContract(ADDRESSES[`TOOLS_${contractIndex}`]);
+  const { contract: stakingContract } = useContract(ADDRESSES[`STAKING_${contractIndex}`]);
+  const { contract: rewardContract } = useContract(ADDRESSES[`REWARDS_${contractIndex}`]);
 
   const { data: ownedFarmers, isLoading: loadingOwnedFarmers } = useOwnedNFTs(farmerContract, address);
   const { data: ownedTools, isLoading: loadingOwnedTools } = useOwnedNFTs(toolsContract, address);
@@ -42,6 +48,11 @@ const Home: NextPage = () => {
 
   return (
     <Container maxW={"container.sm"} px={4} py={6}>
+      <Select onChange={(e) => handleSwitchContract(Number(e.target.value))} value={contractIndex}>
+        <option value={0}>Contract 0</option>
+        <option value={1}>Contract 1</option>
+        {/* 根據需要添加更多選項 */}
+      </Select>
       <FarmerSection ownedFarmers={ownedFarmers} rewardBalance={rewardBalance} />
       <InventorySection ownedTools={ownedTools} loadingOwnedTools={loadingOwnedTools} />
       <EquippedSection equippedTools={equippedTools} />
