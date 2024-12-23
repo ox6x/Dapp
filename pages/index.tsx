1cc3df33909ce651d5e984c897e034cb4f79be0b
ox6x <ox6.x@proton.me>
2024-12-24 04:27:42 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ const Home: NextPage = () => {
     if (rewardBalanceData) {
       setRewardBalance(rewardBalanceData);
     }
-  }, [rewardBalanceData]);
+  }, [rewardBalanceData, contractIndex]); // 添加 contractIndex 作为依赖项
 
   if (!address) {
     return <LoginSection />;
