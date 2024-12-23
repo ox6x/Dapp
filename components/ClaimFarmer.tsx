8a1f254ffe5329e3d2c0bfcae5529f9b64676143
acc0x <ox8xxx@gmail.com>
2024-12-23 14:48:34 +0700

    Update ClaimFarmer.tsx

--- components/ClaimFarmer.tsx
+++ components/ClaimFarmer.tsx
@@ -1,16 +1,19 @@
 import { MediaRenderer, Web3Button, useContract, useContractMetadata } from "@thirdweb-dev/react";
 import { FARMER_ADDRESS } from "../const/addresses";
-import { Box, Container, Flex, Heading } from "@chakra-ui/react";
+import { Box, Container, Flex, Heading, Text } from "@chakra-ui/react";
 
-export function ClaimFarmer() {
+export function ClaimPassport() {
     const { contract } = useContract(FARMER_ADDRESS);
     const { data: metadata } = useContractMetadata(contract);
     
     return (
         <Container maxW={"1200px"}>
             <Flex direction={"column"} alignItems={"center"} justifyContent={"center"} h={"50vh"}>
-                <Heading>Claim your Passport to begin your journey</Heading>
-                <Box borderRadius={"8px"} overflow={"hidden"} my={10}>
+                <Heading textAlign={"center"}>Unlock Your Web3 Mining Adventure</Heading>
+                <Text fontSize={"lg"} color={"gray.600"} mt={4} textAlign={"center"}>
+                    Claim your exclusive NFT Passport to start mining, earning rewards, and exploring the Crypto Farm ecosystem.
+                </Text>
+                <Box borderRadius={"8px"} overflow={"hidden"} my={10} boxShadow={"md"}>
                     <MediaRenderer
                         src={metadata?.image}
                         height="300px"
@@ -21,7 +24,11 @@ export function ClaimFarmer() {
                 <Web3Button
                     contractAddress={FARMER_ADDRESS}
                     action={(contract) => contract.erc1155.claim(0, 1)}
-                >Claim Passport</Web3Button>
+                    colorScheme={"teal"}
+                    size={"lg"}
+                >
+                    Claim Your NFT Passport
+                </Web3Button>
             </Flex>
         </Container>
     );
