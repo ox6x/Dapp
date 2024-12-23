d27cbd185ac1233d1fa6e4047a1e7d9e0eedc830
ox6x <ox6.x@proton.me>
2024-12-23 21:07:23 +0700

    Update InventorySection.tsx

--- components/InventorySection.tsx
+++ components/InventorySection.tsx
@@ -1,13 +1,19 @@
 import { MediaRenderer, useAddress, useContract } from '@thirdweb-dev/react';
-import { STAKING_ADDRESS, TOOLS_ADDRESS } from '../const/addresses';
+import { STAKING_ADDRESS, TOOLS_ADDRESS, STAKING_BB_ADDRESS, TOOLS_BB_ADDRESS } from '../const/addresses';
 import Link from 'next/link';
-import { Text, Box, Button, Card, SimpleGrid, Stack, Flex, Heading, Skeleton } from '@chakra-ui/react';
+import { Text, Box, Button, Card, SimpleGrid, Stack, Flex, Heading, Skeleton, Switch, FormControl, FormLabel } from '@chakra-ui/react';
 import Quantity from './Quantity'; // 引入动态数量选择器组件
+import { useState } from 'react';
 
 const InventorySection = ({ ownedTools, loadingOwnedTools }: any) => {
     const address = useAddress();
-    const { contract: toolContract } = useContract(TOOLS_ADDRESS);
-    const { contract: stakingContract } = useContract(STAKING_ADDRESS);
+    const [useBBAddress, setUseBBAddress] = useState(false);
+
+    const selectedToolAddress = useBBAddress ? TOOLS_BB_ADDRESS : TOOLS_ADDRESS;
+    const selectedStakingAddress = useBBAddress ? STAKING_BB_ADDRESS : STAKING_ADDRESS;
+
+    const { contract: toolContract } = useContract(selectedToolAddress);
+    const { contract: stakingContract } = useContract(selectedStakingAddress);
 
     const handleOnClick = async (id: string, quantity: number) => {
         if (!address) {
@@ -19,13 +25,13 @@ const InventorySection = ({ ownedTools, loadingOwnedTools }: any) => {
             // 检查是否已经授权
             const isApproved = await toolContract?.erc1155.isApproved(
                 address,
-                STAKING_ADDRESS,
+                selectedStakingAddress,
             );
 
             // 如果未授权，先设置授权
             if (!isApproved) {
                 await toolContract?.erc1155.setApprovalForAll(
-                    STAKING_ADDRESS,
+                    selectedStakingAddress,
                     true,
                 );
             }
@@ -44,6 +50,16 @@ const InventorySection = ({ ownedTools, loadingOwnedTools }: any) => {
                 <Heading fontSize="lg" mb={4}>
                     Locker
                 </Heading>
+                <FormControl display="flex" alignItems="center" mb={4}>
+                    <FormLabel htmlFor="contract-switch" mb="0">
+                        Use BB Address
+                    </FormLabel>
+                    <Switch
+                        id="contract-switch"
+                        isChecked={useBBAddress}
+                        onChange={() => setUseBBAddress(!useBBAddress)}
+                    />
+                </FormControl>
                 <Skeleton isLoaded={!loadingOwnedTools}>
                     {ownedTools?.length === 0 ? (
                         <Box textAlign="center" mt={10}>
