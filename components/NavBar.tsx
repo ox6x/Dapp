0522623eb0d0637f318fb30824d8b6472e495ab3
acc0x <ox8xxx@gmail.com>
2024-12-23 18:41:49 +0700

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,40 +1,26 @@
-import { Flex, Link, Box, Text } from "@chakra-ui/react";
+import styles from "./NavBar.module.scss";
 import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
-import styles from "./NavBar.module.scss"; // 引用 SCSS 模块
 
 export default function NavBar() {
-    const address = useAddress(); // 获取钱包地址
+  const address = useAddress(); // 获取钱包地址
 
-    // 如果未连接钱包，不渲染 NavBar
-    if (!address) {
-        return null;
-    }
+  if (!address) {
+    return null; // 如果未连接钱包，不渲染 NavBar
+  }
 
-    return (
-        <Box className={styles.navbar}>
-            <Flex direction={"row"} justifyContent={"space-between"} alignItems={"center"} maxW="1200px" mx="auto">
-                {/* 网站标题 */}
-                <Link href="/" className={styles.navItem} style={{ fontSize: "1.5rem", fontWeight: "bold" }}>
-                    BaseBot
-                </Link>
-
-                {/* 导航链接 */}
-                <Flex direction="row" alignItems="center">
-                    <Link href="/supplier" className={styles.navItem}>
-                        Supplier
-                    </Link>
-                </Flex>
-
-                {/* 连接钱包 */}
-                <Flex alignItems="center">
-                    <Text className={styles.navItem} style={{ marginRight: "1rem" }}>
-                        {address ? `Connected: ${address.slice(0, 6)}...${address.slice(-4)}` : ""}
-                    </Text>
-                    <button className={styles.connectButton}>
-                        <ConnectWallet />
-                    </button>
-                </Flex>
-            </Flex>
-        </Box>
-    );
+  return (
+    <div className={styles.container}>
+      <div className={styles.flex}>
+        <div className={styles.heading}>
+          <a href="/">BaseBot</a>
+        </div>
+        <div className={styles.nav-links}>
+          <a href="/supplier">Supplier</a>
+          <button className="wallet-btn">
+            <ConnectWallet />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
