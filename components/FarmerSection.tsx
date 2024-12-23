1312ac49ba72abd76caaa71b41715c0473c9bdd6
ox6x <ox6.x@proton.me>
2024-12-23 23:51:06 +0700

    Update FarmerSection.tsx

--- components/FarmerSection.tsx
+++ components/FarmerSection.tsx
@@ -1,9 +1,9 @@
-import { Box, Card, Heading, Text } from "@chakra-ui/react";
+import { Box, Card, Heading } from "@chakra-ui/react";
 import { MediaRenderer } from "@thirdweb-dev/react";
-import { ethers } from "ethers";
+import RewardBalances from "./RewardBalances"; // Import the new component
 import './FarmerSection.module.scss';
 
-const FarmerSection = ({ ownedFarmers, rewardBalance, rewardBalanceBB }: any) => (
+const FarmerSection = ({ ownedFarmers, rewardBalances }: any) => (
   <Box mb={6} className="container">
     <Card p={4} className="card">
       <Heading fontSize="lg" mb={4} className="title">
@@ -14,16 +14,7 @@ const FarmerSection = ({ ownedFarmers, rewardBalance, rewardBalanceBB }: any) =>
           <MediaRenderer src={nft.metadata.image} height="150px" width="100%" />
         </Box>
       ))}
-      {rewardBalance && (
-        <Text fontSize={"sm"} fontWeight={"bold"} mb={2} className="text">
-          bBNB Balance: <span className="balance">{ethers.utils.formatUnits(rewardBalance, 18)}</span>
-        </Text>
-      )}
-      {rewardBalanceBB && (
-        <Text fontSize={"sm"} fontWeight={"bold"} mb={2} className="text">
-          BB Balance: <span className="balance">{ethers.utils.formatUnits(rewardBalanceBB, 18)}</span>
-        </Text>
-      )}
+      <RewardBalances rewardBalances={rewardBalances} /> {/* Use the new component */}
     </Card>
   </Box>
 );
