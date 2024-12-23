d36104c8bafbf8b43a0c0219105574e9fe659d45
ox6x <ox6.x@proton.me>
2024-12-24 06:45:54 +0700

    Add files via upload

--- const/addresses.ts
+++ const/addresses.ts
@@ -4,13 +4,12 @@ export const ADDRESSES: { [key: string]: string } = {
   REWARDS_0: '0x0Ad1149eec66A20cB69D114Aec704626C22b7852',
   STAKING_0: '0x8fCE5853B5228093Ab14df76a5a87e60971de989',
   TOOLS_1: '0xf747821D7A019B0C8c11824a141D2EA6De89cA34',
-  REWARDS_1: '0x0Ad1149eec66A20cB69D114Aec704626C22b7852',
+  REWARDS_1: '0x9db1cB30F01074e22cd3D6e92A08C6cB678Ab072',
   STAKING_1: '0x0925c9585398193d1c3327dcd66a69354273DeBF',
 };
 
-export function addAddresses(index: number, addresses: { tools: string, rewards: string, staking: string, farmer: string }) {
+export function addAddresses(index: number, addresses: { tools: string, rewards: string, staking: string }) {
   ADDRESSES[`TOOLS_${index}`] = addresses.tools;
   ADDRESSES[`REWARDS_${index}`] = addresses.rewards;
   ADDRESSES[`STAKING_${index}`] = addresses.staking;
-  ADDRESSES['FARMER'] = addresses.farmer;
 }
