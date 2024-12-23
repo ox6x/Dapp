31e0c999db6528064f100ef54bdd8c9fb6fff405
acc0x <ox8xxx@gmail.com>
2024-12-23 18:30:43 +0700

    Update FarmerSection.tsx

--- components/FarmerSection.tsx
+++ components/FarmerSection.tsx
@@ -1,23 +1,22 @@
 import { Box, Card, Heading, Text } from "@chakra-ui/react";
 import { MediaRenderer } from "@thirdweb-dev/react";
 import { ethers } from "ethers";
+import styles from "./FarmerSection.module.scss"; // 引入 SCSS 模块
 
 const FarmerSection = ({ ownedFarmers, rewardBalance }: any) => (
-  <Box mb={6}>
-    <Card p={4}>
-      <Heading fontSize="lg" mb={4}>
-        Avatar
-      </Heading>
+  <Box className={styles["farmer-section-container"]}>
+    <Card className={styles["farmer-card"]}>
+      <Heading className={styles["farmer-heading"]}>Avatar</Heading>
       {ownedFarmers?.map((nft: any) => (
-        <Box key={nft.metadata.id} borderWidth="1px" borderRadius="lg" overflow="hidden" mb={4}>
+        <Box key={nft.metadata.id} className={styles["farmer-item"]}>
           <MediaRenderer src={nft.metadata.image} height="150px" width="100%" />
         </Box>
       ))}
-      <Text fontSize={"sm"} fontWeight={"bold"} mb={2}>
-        bBNB Balance:
-      </Text>
+      <Text className={styles["farmer-text"]}>bBNB Balance:</Text>
       {rewardBalance && (
-        <Text fontSize={"sm"}>{ethers.utils.formatUnits(rewardBalance, 18)}</Text>
+        <Text className={styles["farmer-reward"]}>
+          {ethers.utils.formatUnits(rewardBalance, 18)}
+        </Text>
       )}
     </Card>
   </Box>
