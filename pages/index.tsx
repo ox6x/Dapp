eb72c828e2c9fa1ae1c6f86c6e49ffc720af6b6a
ox6x <ox6.x@proton.me>
2024-12-24 00:13:22 +0700

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,31 @@
-import { useState } from 'react';
-import { useAddress, useOwnedNFTs } from "@thirdweb-dev/react";
+import { useAddress, useContract, useContractRead, useOwnedNFTs } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
-import { FARMER_ADDRESS } from "../const/addresses";
-import { Container, Select } from "@chakra-ui/react";
+import { FARMER_ADDRESS, REWARDS_ADDRESS, STAKING_ADDRESS, TOOLS_ADDRESS } from "../const/addresses";
 import { ClaimFarmer } from "../components/ClaimFarmer";
-import LoginSection from "../components/LoginSection";
+import { Container } from "@chakra-ui/react";
+
+import LoginSection from "../components/LoginSection"; // 新增導入
 import LoadingScreen from "../components/LoadingScreen";
 import FarmerSection from "../components/FarmerSection";
 import InventorySection from "../components/InventorySection";
 import EquippedSection from "../components/EquippedSection";
-import { Contracts0 } from "../components/contracts/Contracts0";
-import { Contracts1 } from "../components/contracts/Contracts1";
-import RewardBalances from "../components/RewardBalances";  // Import the new component
 
 const Home: NextPage = () => {
-  const [currentContract, setCurrentContract] = useState(0);
   const address = useAddress();
 
-  const contracts = currentContract === 0 ? Contracts0() : Contracts1();
-  const { data: ownedFarmers, isLoading: loadingOwnedFarmers } = useOwnedNFTs(FARMER_ADDRESS, address);
+  const { contract: farmercontract } = useContract(FARMER_ADDRESS);
+  const { contract: toolsContract } = useContract(TOOLS_ADDRESS);
+  const { contract: stakingContract } = useContract(STAKING_ADDRESS);
+  const { contract: rewardContract } = useContract(REWARDS_ADDRESS);
+
+  const { data: ownedFarmers, isLoading: loadingOwnedFarmers } = useOwnedNFTs(farmercontract, address);
+  const { data: ownedTools, isLoading: loadingOwnedTools } = useOwnedNFTs(toolsContract, address);
+
+  const { data: equippedTools } = useContractRead(stakingContract, "getStakeInfo", [address]);
+  const { data: rewardBalance } = useContractRead(rewardContract, "balanceOf", [address]);
 
   if (!address) {
-    return <LoginSection />;
+    return <LoginSection />; // 使用新的 LoginSection 組件
   }
 
   if (loadingOwnedFarmers) {
@@ -36,23 +40,11 @@ const Home: NextPage = () => {
     );
   }
 
-  const rewardBalances = [
-    { label: "bBNB", balance: contracts.rewardBalance0 },
-    { label: "BB", balance: contracts.rewardBalance1 },
-    // Add more reward balances as needed
-  ];
-
   return (
     <Container maxW={"container.sm"} px={4} py={6}>
-      <Select onChange={(e) => setCurrentContract(parseInt(e.target.value, 10))} value={currentContract}>
-        <option value={0}>Contracts 0</option>
-        <option value={1}>Contracts 1</option>
-        {/* You can add more options as you add more contract groups */}
-      </Select>
-      <FarmerSection ownedFarmers={ownedFarmers} />
-      <RewardBalances rewardBalances={rewardBalances} /> {/* Use the new component */}
-      <InventorySection ownedTools={contracts.ownedTools} loadingOwnedTools={contracts.loadingOwnedTools} />
-      <EquippedSection equippedTools={contracts.equippedTools} />
+      <FarmerSection ownedFarmers={ownedFarmers} rewardBalance={rewardBalance} />
+      <InventorySection ownedTools={ownedTools} loadingOwnedTools={loadingOwnedTools} />
+      <EquippedSection equippedTools={equippedTools} />
     </Container>
   );
 };
