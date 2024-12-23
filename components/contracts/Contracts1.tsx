c3a42517a8c9206e2d4d4679b06a140ee6533d18
ox6x <ox6.x@proton.me>
2024-12-23 23:25:30 +0700

    Update Contracts1.tsx

--- components/contracts/Contracts1.tsx
+++ components/contracts/Contracts1.tsx
@@ -1,1 +1,21 @@
+import { useAddress, useContract, useContractRead, useOwnedNFTs } from "@thirdweb-dev/react";
+import { TOOLS_BB_ADDRESS, STAKING_BB_ADDRESS, REWARDS_BB_ADDRESS } from "../../const/addresses";
 
+export const Contracts1 = () => {
+  const address = useAddress();
+
+  const { contract: toolsContract } = useContract(TOOLS_BB_ADDRESS);
+  const { contract: stakingContract } = useContract(STAKING_BB_ADDRESS);
+  const { contract: rewardContract } = useContract(REWARDS_BB_ADDRESS);
+
+  const { data: ownedTools, isLoading: loadingOwnedTools } = useOwnedNFTs(toolsContract, address);
+  const { data: equippedTools } = useContractRead(stakingContract, "getStakeInfo", [address]);
+  const { data: rewardBalance } = useContractRead(rewardContract, "balanceOf", [address]);
+
+  return {
+    ownedTools,
+    loadingOwnedTools,
+    equippedTools,
+    rewardBalance,
+  };
+};
