eaecf8fb76d1f8017f0e2c415b4ecd2c65c0328e
acc0x <ox8xxx@gmail.com>
2024-12-23 20:40:30 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { useAddress, useContract, useContractRead, useOwnedNFTs } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
-import { FARMER_ADDRESS, REWARDS_ADDRESS, STAKING_ADDRESS, TOOLS_ADDRESS } from "../const/addresses";
+import { FARMER_ADDRESS, REWARDS_ADDRESS, STAKING_ADDRESS, TOOLS_ADDRESS, TOOLS_BB_ADDRESS, REWARDS_BB_ADDRESS, STAKING_BB_ADDRESS } from "../const/addresses";
 import { ClaimFarmer } from "../components/ClaimFarmer";
 import { Container } from "@chakra-ui/react";
 
@@ -15,14 +15,20 @@ const Home: NextPage = () => {
 
   const { contract: farmercontract } = useContract(FARMER_ADDRESS);
   const { contract: toolsContract } = useContract(TOOLS_ADDRESS);
+  const { contract: toolsBBContract } = useContract(TOOLS_BB_ADDRESS);
   const { contract: stakingContract } = useContract(STAKING_ADDRESS);
+  const { contract: stakingBBContract } = useContract(STAKING_BB_ADDRESS);
   const { contract: rewardContract } = useContract(REWARDS_ADDRESS);
+  const { contract: rewardsBBContract } = useContract(REWARDS_BB_ADDRESS);
 
   const { data: ownedFarmers, isLoading: loadingOwnedFarmers } = useOwnedNFTs(farmercontract, address);
   const { data: ownedTools, isLoading: loadingOwnedTools } = useOwnedNFTs(toolsContract, address);
+  const { data: ownedToolsBB } = useOwnedNFTs(toolsBBContract, address);
 
   const { data: equippedTools } = useContractRead(stakingContract, "getStakeInfo", [address]);
+  const { data: equippedToolsBB } = useContractRead(stakingBBContract, "getStakeInfo", [address]);
   const { data: rewardBalance } = useContractRead(rewardContract, "balanceOf", [address]);
+  const { data: rewardBalanceBB } = useContractRead(rewardsBBContract, "balanceOf", [address]);
 
   if (!address) {
     return <LoginSection />; // 使用新的 LoginSection 組件
@@ -44,7 +50,9 @@ const Home: NextPage = () => {
     <Container maxW={"container.sm"} px={4} py={6}>
       <FarmerSection ownedFarmers={ownedFarmers} rewardBalance={rewardBalance} />
       <InventorySection ownedTools={ownedTools} loadingOwnedTools={loadingOwnedTools} />
+      <InventorySection ownedTools={ownedToolsBB} loadingOwnedTools={loadingOwnedTools} />
       <EquippedSection equippedTools={equippedTools} />
+      <EquippedSection equippedTools={equippedToolsBB} />
     </Container>
   );
 };
