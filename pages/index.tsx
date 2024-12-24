8107b6f4d8361c3a61fd92497a38d8b67e2949c8
ox6x <ox6.x@proton.me>
2024-12-24 07:44:07 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,26 @@
 import { useAddress, useContract, useContractRead, useOwnedNFTs } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
-import { FARMER_ADDRESS, REWARDS_ADDRESS, STAKING_ADDRESS, TOOLS_ADDRESS } from "../const/addresses";
+import { FARMER_ADDRESS, REWARDS_ADDRESS, STAKING_ADDRESS, TOOLS_ADDRESS, setVersion } from "../const/addresses";
 import { ClaimFarmer } from "../components/ClaimFarmer";
-import { Container } from "@chakra-ui/react";
+import { Container, Select } from "@chakra-ui/react";
 
 import LoginSection from "../components/LoginSection"; // 新增導入
 import LoadingScreen from "../components/LoadingScreen";
 import FarmerSection from "../components/FarmerSection";
 import InventorySection from "../components/InventorySection";
 import EquippedSection from "../components/EquippedSection";
+import { useState } from "react";
 
 const Home: NextPage = () => {
+  const [version, setVersionState] = useState('V1');
   const address = useAddress();
+
+  const handleVersionChange = (event) => {
+    const newVersion = event.target.value;
+    setVersionState(newVersion);
+    setVersion(newVersion);
+    window.location.reload(); // 重新加载页面以应用更改
+  };
 
   const { contract: farmercontract } = useContract(FARMER_ADDRESS);
   const { contract: toolsContract } = useContract(TOOLS_ADDRESS);
@@ -25,7 +34,7 @@ const Home: NextPage = () => {
   const { data: rewardBalance } = useContractRead(rewardContract, "balanceOf", [address]);
 
   if (!address) {
-    return <LoginSection />; // 使用新的 LoginSection 組件
+    return <LoginSection />;
   }
 
   if (loadingOwnedFarmers) {
@@ -42,6 +51,10 @@ const Home: NextPage = () => {
 
   return (
     <Container maxW={"container.sm"} px={4} py={6}>
+      <Select value={version} onChange={handleVersionChange} width="fit-content" mb={4}>
+        <option value="V1">V1</option>
+        <option value="V2">V2</option>
+      </Select>
       <FarmerSection ownedFarmers={ownedFarmers} rewardBalance={rewardBalance} />
       <InventorySection ownedTools={ownedTools} loadingOwnedTools={loadingOwnedTools} />
       <EquippedSection equippedTools={equippedTools} />
