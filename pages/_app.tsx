3519ac774bc78424b0e476e974d0dad66086f8c6
ox6x <ox6.x@proton.me>
2024-12-24 15:54:48 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import { ThirdwebProvider, ConnectWallet } from "@thirdweb-dev/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import NavBar from "../components/NavBar"; // 確保路徑正確
+import '../styles/globals.scss'; // 導入全局 SCSS 文件
+
 const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID as string;
 
 export default function MyApp({ Component, pageProps }: AppProps) {
