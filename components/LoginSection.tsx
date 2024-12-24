c223d1b2906ee19403784acf6a029f787c717e3b
ox6x <ox6.x@proton.me>
2024-12-24 22:59:27 +0700

    Update LoginSection.tsx

--- components/LoginSection.tsx
+++ components/LoginSection.tsx
@@ -10,14 +10,14 @@ const LoginSection = () => {
         <h1 className={styles.title}>Welcome to Your DApp</h1>
         {/* 描述文字 */}
         <p className={styles.description}>
-          Connect your wallet to securely access your dashboard and manage your digital assets.
+          Connect your wallet to access your dashboard and manage your digital assets with ease.
         </p>
         {/* Connect Wallet 按鈕 */}
         <div className={styles.form}>
           <ConnectWallet
             className={styles.connectWalletButton}
-            theme="light" /* 按鈕主題 */
-            btnTitle="Connect Wallet" /* 自定義按鈕標題 */
+            theme="light"
+            btnTitle="Connect Wallet"
           />
         </div>
       </div>
