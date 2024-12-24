ed91352dfc1cf5e3bb8b6b28e7efffca4c4f33ed
ox6x <ox6.x@proton.me>
2024-12-24 13:26:43 +0700

    Update addresses.ts

--- const/addresses.ts
+++ const/addresses.ts
@@ -1,24 +1,31 @@
 type Version = 'V1' | 'V2';
 
-let ADDRESS_VERSION: Version = (typeof window !== 'undefined' ? localStorage.getItem('ADDRESS_VERSION') as Version : 'V1') || 'V1';
+let ADDRESS_VERSION: Version = (() => {
+  if (typeof window !== 'undefined') {
+    const storedVersion = localStorage.getItem('ADDRESS_VERSION') as Version;
+    return storedVersion === 'V1' || storedVersion === 'V2' ? storedVersion : 'V1';
+  }
+  return 'V1';
+})();
 
 const versionAddresses: Record<Version, { TOOLS_ADDRESS: string; REWARDS_ADDRESS: string; STAKING_ADDRESS: string }> = {
   V1: {
     TOOLS_ADDRESS: '0x605f710b66Cc10A0bc0DE7BD8fe786D5C9719179',
     REWARDS_ADDRESS: '0x0Ad1149eec66A20cB69D114Aec704626C22b7852',
-    STAKING_ADDRESS: '0x8fCE5853B5228093Ab14df76a5a87e60971de989'
+    STAKING_ADDRESS: '0x8fCE5853B5228093Ab14df76a5a87e60971de989',
   },
   V2: {
     TOOLS_ADDRESS: '0xf747821D7A019B0C8c11824a141D2EA6De89cA34',
     REWARDS_ADDRESS: '0x9db1cB30F01074e22cd3D6e92A08C6cB678Ab072',
-    STAKING_ADDRESS: '0x0925c9585398193d1c3327dcd66a69354273DeBF'
-  }
+    STAKING_ADDRESS: '0x0925c9585398193d1c3327dcd66a69354273DeBF',
+  },
 };
 
 export const FARMER_ADDRESS = '0xf773A055Ec0Ab4D1404dBf61178f7dA7D769C44f';
-export const TOOLS_ADDRESS = versionAddresses[ADDRESS_VERSION].TOOLS_ADDRESS;
-export const REWARDS_ADDRESS = versionAddresses[ADDRESS_VERSION].REWARDS_ADDRESS;
-export const STAKING_ADDRESS = versionAddresses[ADDRESS_VERSION].STAKING_ADDRESS;
+
+export const getToolsAddress = () => versionAddresses[ADDRESS_VERSION].TOOLS_ADDRESS;
+export const getRewardsAddress = () => versionAddresses[ADDRESS_VERSION].REWARDS_ADDRESS;
+export const getStakingAddress = () => versionAddresses[ADDRESS_VERSION].STAKING_ADDRESS;
 
 export const setVersion = (version: Version) => {
   ADDRESS_VERSION = version;
