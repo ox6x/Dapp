e2696fbf60260092ef4b3ee4220e77e5eac39a0f
ox6x <ox6.x@proton.me>
2024-12-25 06:08:22 +0700

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,12 +1,39 @@
-import { Container, Flex, Heading, Link, Drawer, DrawerBody, DrawerOverlay, DrawerContent, DrawerCloseButton, useDisclosure, IconButton } from "@chakra-ui/react";
-import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
+import { 
+    Container, 
+    Flex, 
+    Heading, 
+    Link, 
+    Drawer, 
+    DrawerBody, 
+    DrawerOverlay, 
+    DrawerContent, 
+    DrawerCloseButton, 
+    useDisclosure, 
+    IconButton, 
+    Box, 
+    Text, 
+    Spinner 
+} from "@chakra-ui/react";
+import { ConnectWallet, useAddress, useContract, useOwnedNFTs } from "@thirdweb-dev/react";
 import { FaWallet } from "react-icons/fa";
 import styles from './NavBar.module.scss';
+
+const NFT_CONTRACTS = [
+    "0x605f710b66Cc10A0bc0DE7BD8fe786D5C9719179", // 合約地址 1
+    "0xf747821D7A019B0C8c11824a141D2EA6De89cA34", // 合約地址 2
+];
 
 export default function NavBar() {
     const address = useAddress();
     const { isOpen, onOpen, onClose } = useDisclosure();
 
+    // 獲取所有合約的實例
+    const contracts = NFT_CONTRACTS.map((contractAddress) => useContract(contractAddress));
+
+    // 統一處理多個合約的 NFT 資料
+    const ownedNFTsList = contracts.map(({ contract }) => useOwnedNFTs(contract, address));
+
+    // 確保地址已連接
     if (!address) {
         return null;
     }
@@ -23,7 +50,6 @@ export default function NavBar() {
                     <Link href="/supplier" className={styles.link}>
                         Supplier
                     </Link>
-                    {/* 隱藏的按鈕 */}
                     <IconButton 
                         icon={<FaWallet />} 
                         aria-label="Open Wallet" 
@@ -34,13 +60,61 @@ export default function NavBar() {
                 </Flex>
             </Flex>
 
-            {/* Drawer for Connect Wallet */}
+            {/* Drawer for Connect Wallet and NFTs */}
             <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
                 <DrawerOverlay />
-                <DrawerContent width="350px"> {/* 設置寬度為 350px */}
+                <DrawerContent width="350px">
                     <DrawerCloseButton />
                     <DrawerBody display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start" pt={10}>
                         <ConnectWallet className={styles.connectWallet} />
+                        <Box mt={6} w="100%">
+                            <Heading size="md" textAlign="center" mb={4}>
+                                Your NFTs
+                            </Heading>
+                            {NFT_CONTRACTS.map((contractAddress, index) => {
+                                const { data: ownedNFTs, isLoading } = ownedNFTsList[index];
+                                return (
+                                    <Box key={contractAddress} mt={4}>
+                                        <Heading size="sm" textAlign="center" mb={2}>
+                                            Contract: {contractAddress}
+                                        </Heading>
+                                        {isLoading ? (
+                                            <Spinner size="lg" />
+                                        ) : ownedNFTs && ownedNFTs.length > 0 ? (
+                                            ownedNFTs.map((nft) => (
+                                                <Box
+                                                    key={`${contractAddress}-${nft.metadata.id}`}
+                                                    border="1px solid #e2e8f0"
+                                                    borderRadius="md"
+                                                    p={4}
+                                                    mb={4}
+                                                    textAlign="center"
+                                                >
+                                                    <img
+                                                        src={nft.metadata.image}
+                                                        alt={nft.metadata.name}
+                                                        style={{
+                                                            width: "100px",
+                                                            height: "100px",
+                                                            borderRadius: "8px",
+                                                            margin: "0 auto",
+                                                        }}
+                                                    />
+                                                    <Text fontWeight="bold" mt={2}>
+                                                        {nft.metadata.name}
+                                                    </Text>
+                                                    <Text fontSize="sm" color="gray.500">
+                                                        Token ID: {nft.metadata.id}
+                                                    </Text>
+                                                </Box>
+                                            ))
+                                        ) : (
+                                            <Text>No NFTs found.</Text>
+                                        )}
+                                    </Box>
+                                );
+                            })}
+                        </Box>
                     </DrawerBody>
                 </DrawerContent>
             </Drawer>
