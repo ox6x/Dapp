7fa52d4c3f5d31a284ae43c2963321d04b79901f
ox6x <ox6.x@proton.me>
2024-12-25 02:10:11 +0700

    Update FarmerSection.tsx

--- components/FarmerSection.tsx
+++ components/FarmerSection.tsx
@@ -1,25 +1,59 @@
-import { Box, Card, Heading, Text } from "@chakra-ui/react";
+import { Box, Card, Heading, Text, Stack, Flex } from "@chakra-ui/react";
 import { MediaRenderer } from "@thirdweb-dev/react";
 import { ethers } from "ethers";
-import styles from './FarmerSection.module.scss';
+import styles from "./FarmerSection.module.scss";
 
 const FarmerSection = ({ ownedFarmers, rewardBalance }: any) => (
-  <Box className={styles.farmerSection}>
-    <Card className={styles.card}>
-      <Heading className={styles.heading}>
+  <Box className={styles.farmerSection} p={4}>
+    <Card className={styles.card} p={6}>
+      <Heading className={styles.heading} mb={4}>
         Avatar
       </Heading>
-      {ownedFarmers?.map((nft: any) => (
-        <Box key={nft.metadata.id} className={styles.nftBox}>
-          <MediaRenderer src={nft.metadata.image} className={styles.nftImage} />
-        </Box>
-      ))}
-      <Text className={styles.balanceLabel}>
-        bBNB Balance:
-      </Text>
-      {rewardBalance && (
-        <Text className={styles.balanceValue}>{ethers.utils.formatUnits(rewardBalance, 18)}</Text>
-      )}
+      <Stack spacing={6} className={styles.avatars}>
+        {ownedFarmers?.map((nft: any) => (
+          <Flex
+            key={nft.metadata.id}
+            className={styles.nftBox}
+            direction="column"
+            align="center"
+            gap={4}
+          >
+            <MediaRenderer
+              src={nft.metadata.image}
+              alt={nft.metadata.name || "Farmer Avatar"}
+              className={styles.nftImage}
+              style={{
+                width: "150px",
+                height: "150px",
+                borderRadius: "8px",
+                objectFit: "cover",
+              }}
+            />
+            <Text className={styles.nftName} fontWeight="bold" fontSize="lg">
+              {nft.metadata.name || "Unknown"}
+            </Text>
+          </Flex>
+        ))}
+      </Stack>
+      <Box className={styles.balanceSection} mt={6}>
+        <Text className={styles.balanceLabel} fontWeight="medium" fontSize="md">
+          bBNB Balance:
+        </Text>
+        {rewardBalance ? (
+          <Text
+            className={styles.balanceValue}
+            fontWeight="bold"
+            fontSize="lg"
+            color="green.500"
+          >
+            {ethers.utils.formatUnits(rewardBalance, 18)}
+          </Text>
+        ) : (
+          <Text className={styles.balanceValue} fontWeight="medium" color="gray.500">
+            0
+          </Text>
+        )}
+      </Box>
     </Card>
   </Box>
 );
