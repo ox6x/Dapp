2e5667751c7eeae96a8c1747900064ffb4828b02
ox6x <ox6.x@proton.me>
2024-12-24 13:19:01 +0700

    Update ContractContext.d.ts

--- contexts/ContractContext.d.ts
+++ contexts/ContractContext.d.ts
@@ -1,1 +1,8 @@
- 
+ declare module "../contexts/ContractContext" {
+  import { ReactNode } from "react";
+  export const ContractProvider: ({ children }: { children: ReactNode }) => JSX.Element;
+  export const useContractState: () => {
+    state: { version: "V1" | "V2"; contract: any };
+    dispatch: React.Dispatch<any>;
+  };
+}
