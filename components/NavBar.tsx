c3b651d5e804c44fcc08906c921feca30c223a61
ox6x <ox6.x@proton.me>
2024-12-25 04:09:52 +0700

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,27 +1,15 @@
-import {
-  Container,
-  Flex,
-  Heading,
-  Link,
-  IconButton,
-  Box,
-} from "@chakra-ui/react";
+import { Container, Flex, Heading, Link, Drawer, DrawerBody, DrawerOverlay, DrawerContent, DrawerCloseButton, useDisclosure, Button, IconButton } from "@chakra-ui/react";
 import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
 import { FaWallet } from "react-icons/fa";
-import { useState } from "react";
 import styles from './NavBar.module.scss';
 
 export default function NavBar() {
     const address = useAddress();
-    const [isOpen, setIsOpen] = useState(false);
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     if (!address) {
         return null;
     }
-
-    const toggleWallet = () => {
-        setIsOpen(!isOpen);
-    };
 
     return (
         <Container className={styles.navBarContainer}>
@@ -35,39 +23,27 @@ export default function NavBar() {
                     <Link href="/supplier" className={styles.link}>
                         Supplier
                     </Link>
-                    {/* 錢包圖標，點擊後顯示 ConnectWallet */}
+                    {/* 隱藏的按鈕 */}
                     <IconButton 
                         icon={<FaWallet />} 
                         aria-label="Open Wallet" 
-                        onClick={toggleWallet} 
+                        onClick={onOpen} 
                         className={styles.walletIconButton} 
                         variant="ghost"
                     />
                 </Flex>
             </Flex>
 
-            {/* 顯示 ConnectWallet 當 isOpen 為 true 時 */}
-            {isOpen && (
-                <Box
-                    className={styles.walletBox}
-                    display="flex"
-                    flexDirection="column"
-                    alignItems="center"
-                    justifyContent="center"
-                    p={6}
-                    boxShadow="0 4px 12px rgba(0, 0, 0, 0.1)"
-                    borderRadius="8px"
-                    position="absolute"
-                    top="50%"
-                    left="50%"
-                    transform="translate(-50%, -50%)"
-                    backgroundColor="white"
-                >
-                    <ConnectWallet className={styles.connectWallet} />
-                    {/* 可選：添加一個關閉按鈕 */}
-                    <button onClick={toggleWallet}>Close</button>
-                </Box>
-            )}
+            {/* Drawer for Connect Wallet */}
+            <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+                <DrawerOverlay />
+                <DrawerContent>
+                    <DrawerCloseButton />
+                    <DrawerBody display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start" pt={10}>
+                        <ConnectWallet className={styles.connectWallet} />
+                    </DrawerBody>
+                </DrawerContent>
+            </Drawer>
         </Container>
     );
 }
