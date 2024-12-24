b725b5e2bc5f66612e29c1e62105d33d5b02d394
ox6x <ox6.x@proton.me>
2024-12-24 13:27:14 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,33 @@
-import { ThirdwebProvider, ConnectWallet } from "@thirdweb-dev/react";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-import NavBar from "../components/NavBar"; // 確保路徑正確
+import NavBar from "../components/NavBar"; // 确保路径正确
 import { ContractProvider } from "../contexts/ContractContext"; // Import the ContractProvider
+
 const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID as string;
+
+if (!CLIENT_ID) {
+  throw new Error("NEXT_PUBLIC_CLIENT_ID is not defined in your environment variables.");
+}
+
+const activeChain = {
+  chainId: 97,
+  rpc: ["https://97.rpc.thirdweb.com"],
+  nativeCurrency: {
+    name: "Binance Smart Chain Testnet",
+    symbol: "tBNB",
+    decimals: 18,
+  },
+  name: "BSC Testnet",
+  chain: "bsc-testnet",
+  shortName: "tBNB",
+  testnet: true,
+  slug: "bsc-testnet",
+};
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider
-      clientId={CLIENT_ID}
-      activeChain={{
-        chainId: 97,
-        rpc: ["https://97.rpc.thirdweb.com"],
-        nativeCurrency: {
-          name: "Binance Smart Chain Testnet",
-          symbol: "tBNB",
-          decimals: 18,
-        },
-        name: "BSC Testnet",
-        chain: "bsc-testnet",
-        shortName: "tBNB",
-        testnet: true,
-        slug: "bsc-testnet",
-      }}
-    >
+    <ThirdwebProvider clientId={CLIENT_ID} activeChain={activeChain}>
       <ContractProvider>
         <ChakraProvider>
           <NavBar />
