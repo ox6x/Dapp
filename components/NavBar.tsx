7763b74beb12e83cf98f148132fbd7241520df65
ox6x <ox6.x@proton.me>
2024-12-25 02:51:43 +0700

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,58 +1,29 @@
-import { useState } from "react";
-import {
-  Container,
-  Flex,
-  Heading,
-  Link,
-  Button,
-  Box,
-  Collapse,
-} from "@chakra-ui/react";
+import { Container, Flex, Heading, Link } from "@chakra-ui/react";
 import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
-import styles from "./NavBar.module.scss";
+import styles from './NavBar.module.scss';
 
 export default function NavBar() {
-  const address = useAddress();
-  const [isPanelOpen, setPanelOpen] = useState(false);
+    const address = useAddress();
 
-  if (!address) {
-    return null;
-  }
+    if (!address) {
+        return null;
+    }
 
-  const togglePanel = () => setPanelOpen(!isPanelOpen);
-
-  return (
-    <Container className={styles.navBarContainer}>
-      <Flex className={styles.navBarFlex}>
-        <Heading className={styles.heading}>
-          <Link href="/" style={{ textDecoration: "none" }}>
-            BaseBot
-          </Link>
-        </Heading>
-        <Flex alignItems="center" justifyContent="flex-end" w="auto">
-          <Link href="/supplier" className={styles.link}>
-            Supplier
-          </Link>
-          <Button onClick={togglePanel} size="sm" ml={4} colorScheme="blue">
-            {isPanelOpen ? "Close Wallet Panel" : "Wallet Panel"}
-          </Button>
-        </Flex>
-      </Flex>
-
-      {/* 折疊式 Wallet Panel */}
-      <Collapse in={isPanelOpen} animateOpacity>
-        <Box
-          mt={4}
-          p={4}
-          rounded="md"
-          shadow="md"
-          bg="gray.50"
-          borderWidth="1px"
-          borderColor="gray.200"
-        >
-          <ConnectWallet />
-        </Box>
-      </Collapse>
-    </Container>
-  );
+    return (
+        <Container className={styles.navBarContainer}>
+            <Flex className={styles.navBarFlex}>
+                <Heading className={styles.heading}>
+                    <Link href="/" style={{ textDecoration: "none" }}>
+                        BaseBot
+                    </Link>
+                </Heading>
+                <Flex alignItems={"center"} justifyContent={"flex-end"} w="auto">
+                    <Link href="/supplier" className={styles.link}>
+                        Supplier
+                    </Link>
+                    <ConnectWallet className={styles.connectWallet} />
+                </Flex>
+            </Flex>
+        </Container>
+    );
 }
